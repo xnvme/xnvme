60e5c9e3141a104834acd2b81842da46e17a9f42
Simon A. F. Lund <simon.lund@samsung.com>
2019-12-11 16:52:30 +0100

    scripts: added source-archive generator
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ scripts/xnvme_gen_src_archive.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# This script creates a source-archive of the given git-version-tag on which to
+# base .rpm and .debs on. It uses 'git-archive-all' to do the heavy sub-module
+# stuff.
+#
+# Use naming conventions compatible with expectations of Debian upstream source
+# archives
+#
+PROJECT=xnvme
+MAJOR=0
+MINOR=0
+PATCH=10
+VERSION="${MAJOR}.${MINOR}.${PATCH}"
+
+REF="v${VERSION}"
+
+PREFIX="${PROJECT}-${VERSION}"
+DEST="build/${PREFIX}.src.tar.gz"
+
+# Check that git-archive-all is installed
+if ! git-archive-all --help &> /dev/null; then
+  echo "# This script uses 'git-archive-all', please install:"
+  echo "# pip install git-archive-all"
+  exit 1
+fi
+
+# Check that generator is run from xNVMe repos-root
+DLIST="tools cmake docs examples include scripts src tests third-party"
+for DNAME in $DLIST; do
+  if [[ ! -d "$DNAME" ]]; then
+    echo "# FAILED: could not find dir($DNAME)"
+    echo "# INFO: script('$0'); must be executed from repository root"
+    exit 1
+  fi
+done
+
+# Now create the archive!
+git-archive-all --prefix="${PREFIX}" --force-submodules $DEST
+
+echo "# $0: DONE!"
+ls -lh $DEST
