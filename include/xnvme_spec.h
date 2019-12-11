e8fd398d9b8e56e5fb50bbaaaf7a395c2a670190
Simon A. F. Lund <simon.lund@samsung.com>
2019-12-11 16:54:20 +0100

    spec: added implementation of 'xnvme_spec_*'
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ include/xnvme_spec.h
@@ -0,0 +1,7 @@
+// Copyright (C) Simon A. F. Lund <simon.lund@samsung.com>
+// SPDX-License-Identifier: Apache-2.0
+#ifndef __INTERNAL_XNVME_SPEC_H
+#define __INTERNAL_XNVME_SPEC_H
+
+
+#endif /* __INTERNAL_XNVME_SPEC_H */
