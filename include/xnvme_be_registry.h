e1728e364ef7ca70542d4b189306ff47b5a09d8d
Simon A. F. Lund <simon.lund@samsung.com>
2019-12-18 13:28:54 +0100

    be:registry:internal: added backend registry header
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ include/xnvme_be_registry.h
@@ -0,0 +1,11 @@
+// Copyright (C) Simon A. F. Lund <simon.lund@samsung.com>
+// SPDX-License-Identifier: Apache-2.0
+#ifndef __INTERNAL_XNVME_BE_REGISTRY_H
+#define __INTERNAL_XNVME_BE_REGISTRY_H
+
+extern struct xnvme_be xnvme_be_fioc;
+extern struct xnvme_be xnvme_be_lioc;
+extern struct xnvme_be xnvme_be_liou;
+extern struct xnvme_be xnvme_be_spdk;
+
+#endif /* __INTERNAL_XNVME_BE_REGISTRY_H */
