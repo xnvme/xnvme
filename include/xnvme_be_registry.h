6924d285ac338fa5ec44da35ab9bd68ebdaa508e
Simon A. F. Lund <simon.lund@samsung.com>
2019-12-18 13:36:54 +0100

    be:registry: added nosys
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- include/xnvme_be_registry.h
+++ include/xnvme_be_registry.h
@@ -3,6 +3,7 @@
 #ifndef __INTERNAL_XNVME_BE_REGISTRY_H
 #define __INTERNAL_XNVME_BE_REGISTRY_H
 
+extern struct xnvme_be xnvme_be_nosys;
 extern struct xnvme_be xnvme_be_fioc;
 extern struct xnvme_be xnvme_be_lioc;
 extern struct xnvme_be xnvme_be_liou;
