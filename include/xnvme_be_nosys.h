083ced3fcaf3746e112f609905325c598a8eec40
Simon A. F. Lund <simon.lund@samsung.com>
2020-01-10 13:50:41 +0100

    be:nosys: added the 'Not Supported' backend
    This backend provides a set of functions matching the
    backend-function-interface definitions. The functions do nothing but
    return -ENOSYS.
    
    The intend is that these helpers are setup on backends which are not
    "ENABLED". Also, for example, an entirely synchronous backend can use the
    'xnvme_be_nosys_async_*' helpers in their setup of the backend struct.
    
    The alternate strategy would be to check function-pointers before
    calling them, or even setup the function interface on behalf of the
    backend. However, as things change, segfaults are easy to catch.
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ include/xnvme_be_nosys.h
@@ -0,0 +1,78 @@
+// Copyright (C) Simon A. F. Lund <simon.lund@samsung.com>
+// SPDX-License-Identifier: Apache-2.0
+#ifndef __INTERNAL_XNVME_BE_NOSYS_H
+#define __INTERNAL_XNVME_BE_NOSYS_H
+
+extern struct xnvme_be xnvme_be_nosys;
+
+int
+xnvme_be_nosys_enumerate(struct xnvme_enumeration *list, int opts);
+
+int
+xnvme_be_nosys_dev_from_ident(const struct xnvme_ident *ident,
+			      struct xnvme_dev **dev);
+
+void
+xnvme_be_nosys_dev_close(struct xnvme_dev *dev);
+
+void *
+xnvme_be_nosys_buf_alloc(const struct xnvme_dev *dev, size_t nbytes,
+			 uint64_t *phys);
+
+int
+xnvme_be_nosys_buf_vtophys(const struct xnvme_dev *dev, void *buf,
+			   uint64_t *phys);
+
+void *
+xnvme_be_nosys_buf_realloc(const struct xnvme_dev *dev, void *buf,
+			   size_t nbytes, uint64_t *phys);
+
+void
+xnvme_be_nosys_buf_free(const struct xnvme_dev *dev, void *buf);
+
+int
+xnvme_be_nosys_async_init(struct xnvme_dev *dev, struct xnvme_async_ctx **ctx,
+			  uint16_t depth);
+
+int
+xnvme_be_nosys_async_term(struct xnvme_async_ctx *ctx);
+
+int
+xnvme_be_nosys_async_poke(struct xnvme_async_ctx *ctx,
+			  uint32_t max);
+
+int
+xnvme_be_nosys_async_wait(struct xnvme_async_ctx *ctx);
+
+int
+xnvme_be_nosys_cmd_pass(struct xnvme_dev *dev, struct xnvme_spec_cmd *cmd,
+			void *dbuf, size_t dbuf_nbytes, void *mbuf,
+			size_t mbuf_nbytes, int opts, struct xnvme_req *req);
+
+int
+xnvme_be_nosys_cmd_pass_admin(struct xnvme_dev *dev, struct xnvme_spec_cmd *cmd,
+			      void *dbuf, size_t dbuf_nbytes, void *mbuf,
+			      size_t mbuf_nbytes, int opts,
+			      struct xnvme_req *req);
+
+#define XNVME_BE_NOSYS_FUNC {					\
+	.cmd_pass = xnvme_be_nosys_cmd_pass,			\
+	.cmd_pass_admin = xnvme_be_nosys_cmd_pass_admin,	\
+								\
+	.async_init = xnvme_be_nosys_async_init,		\
+	.async_term = xnvme_be_nosys_async_term,		\
+	.async_poke = xnvme_be_nosys_async_poke,		\
+	.async_wait = xnvme_be_nosys_async_wait,		\
+								\
+	.buf_alloc = xnvme_be_nosys_buf_alloc,			\
+	.buf_vtophys = xnvme_be_nosys_buf_vtophys,		\
+	.buf_realloc = xnvme_be_nosys_buf_realloc,		\
+	.buf_free = xnvme_be_nosys_buf_free,			\
+								\
+	.enumerate = xnvme_be_nosys_enumerate,			\
+								\
+	.dev_from_ident = xnvme_be_nosys_dev_from_ident,	\
+	.dev_close = xnvme_be_nosys_dev_close,			\
+}
+
+#endif /* __INTERNAL_XNVME_BE_NOSYS_H */
---
+++ src/xnvme_be_nosys.c
@@ -0,0 +1,142 @@
+// Copyright (C) Simon A. F. Lund <simon.lund@samsung.com>
+// SPDX-License-Identifier: Apache-2.0
+#include <errno.h>
+#include <libxnvme.h>
+#include <xnvme_be.h>
+#include <xnvme_be_nosys.h>
+
+int
+xnvme_be_nosys_enumerate(struct xnvme_enumeration *XNVME_UNUSED(list),
+			 int XNVME_UNUSED(opts))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	return -ENOSYS;
+}
+
+int
+xnvme_be_nosys_dev_from_ident(const struct xnvme_ident *XNVME_UNUSED(ident),
+			      struct xnvme_dev **XNVME_UNUSED(dev))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	return -ENOSYS;
+}
+
+void
+xnvme_be_nosys_dev_close(struct xnvme_dev *XNVME_UNUSED(dev))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	errno = ENOSYS;
+	return;
+}
+
+int
+xnvme_be_nosys_async_init(struct xnvme_dev *XNVME_UNUSED(dev),
+			  struct xnvme_async_ctx **XNVME_UNUSED(ctx),
+			  uint16_t XNVME_UNUSED(depth))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	return -ENOSYS;
+}
+
+int
+xnvme_be_nosys_async_term(struct xnvme_async_ctx *XNVME_UNUSED(ctx))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	return -ENOSYS;
+}
+
+int
+xnvme_be_nosys_async_poke(struct xnvme_async_ctx *XNVME_UNUSED(ctx),
+			  uint32_t XNVME_UNUSED(max))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	return -ENOSYS;
+}
+
+int
+xnvme_be_nosys_async_wait(struct xnvme_async_ctx *XNVME_UNUSED(ctx))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	return -ENOSYS;
+}
+
+void *
+xnvme_be_nosys_buf_alloc(const struct xnvme_dev *XNVME_UNUSED(dev),
+			 size_t XNVME_UNUSED(nbytes),
+			 uint64_t *XNVME_UNUSED(phys))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	errno = ENOSYS;
+	return NULL;
+}
+
+void *
+xnvme_be_nosys_buf_realloc(const struct xnvme_dev *XNVME_UNUSED(dev),
+			   void *XNVME_UNUSED(buf), size_t XNVME_UNUSED(nbytes),
+			   uint64_t *XNVME_UNUSED(phys))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	errno = ENOSYS;
+	return NULL;
+}
+
+void
+xnvme_be_nosys_buf_free(const struct xnvme_dev *XNVME_UNUSED(dev),
+			void *XNVME_UNUSED(buf))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+}
+
+int
+xnvme_be_nosys_buf_vtophys(const struct xnvme_dev *XNVME_UNUSED(dev),
+			   void *XNVME_UNUSED(buf),
+			   uint64_t *XNVME_UNUSED(phys))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	return -ENOSYS;
+}
+
+int
+xnvme_be_nosys_cmd_pass(struct xnvme_dev *XNVME_UNUSED(dev),
+			struct xnvme_spec_cmd *XNVME_UNUSED(cmd),
+			void *XNVME_UNUSED(dbuf),
+			size_t XNVME_UNUSED(dbuf_nbytes),
+			void *XNVME_UNUSED(mbuf),
+			size_t XNVME_UNUSED(mbuf_nbytes),
+			int XNVME_UNUSED(flags),
+			struct xnvme_req *XNVME_UNUSED(req))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	return -ENOSYS;
+}
+
+int
+xnvme_be_nosys_cmd_pass_admin(struct xnvme_dev *XNVME_UNUSED(dev),
+			      struct xnvme_spec_cmd *XNVME_UNUSED(cmd),
+			      void *XNVME_UNUSED(dbuf),
+			      size_t XNVME_UNUSED(dbuf_nbytes),
+			      void *XNVME_UNUSED(mbuf),
+			      size_t XNVME_UNUSED(mbuf_nbytes),
+			      int XNVME_UNUSED(flags),
+			      struct xnvme_req *XNVME_UNUSED(req))
+{
+	XNVME_DEBUG("FAILED: not implemented(possibly intentional)");
+	return -ENOSYS;
+}
+
+static const char *g_schemes[] = {
+	"nosys",
+};
+
+#define XNVME_BE_NOSYS_ATTR {					\
+	.name = "nosys",					\
+	.enabled = 1,						\
+	.schemes = g_schemes,					\
+	.nschemes = sizeof g_schemes / sizeof(*g_schemes),	\
+}
+
+struct xnvme_be xnvme_be_nosys = {
+	.func = XNVME_BE_NOSYS_FUNC,
+	.attr = XNVME_BE_NOSYS_ATTR,
+	.state = { 0 },
+};
