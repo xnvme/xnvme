e09b0d3230c1b589b8e1aa9e9cb590f51efbe5ad
Simon A. F. Lund <simon.lund@samsung.com>
2020-04-01 21:56:21 +0200

    be:registry: added 'be:liou'
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- include/xnvme_be_registry.h
+++ include/xnvme_be_registry.h
@@ -6,5 +6,6 @@
 extern struct xnvme_be xnvme_be_spdk;
 extern struct xnvme_be xnvme_be_fioc;
 extern struct xnvme_be xnvme_be_lioc;
+extern struct xnvme_be xnvme_be_liou;
 
 #endif /* __INTERNAL_XNVME_BE_REGISTRY_H */
