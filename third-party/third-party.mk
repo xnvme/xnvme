74190650a5ce93bf5951da62698ed1b71d642c5a
Simon A. F. Lund <simon.lund@samsung.com>
2020-04-02 15:41:12 +0200

    3p:mk: including mk-file for 'fioe' dependency
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- third-party/third-party.mk
+++ third-party/third-party.mk
@@ -3,18 +3,21 @@
 #
 include third-party/spdk.mk
 include third-party/liburing.mk
+include third-party/fio.mk
 
 .PHONY: third-party-clean
 third-party-clean:
 	@echo "## xNVMe: make third-party-clean"
 	$(MAKE) third-party-spdk-clean
 	$(MAKE) third-party-liburing-clean
+	$(MAKE) third-party-fio-clean
 
 .PHONY: third-party-clobber
 third-party-clobber: third-party-clean
 	@echo "## xNVMe: make third-party-clobber"
 	$(MAKE) third-party-spdk-clobber
 	$(MAKE) third-party-liburing-clobber
+	$(MAKE) third-party-fio-clobber
 
 .PHONY: third-party-update
 third-party-update:
