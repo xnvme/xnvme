cd6f9b55582880fa4ba86295eed8fd96b6db0102
Simon A. F. Lund <simon.lund@samsung.com>
2020-04-02 15:40:17 +0200

    3p:mk: including mk-file for 'be:liou' dependency
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- third-party/third-party.mk
+++ third-party/third-party.mk
@@ -2,16 +2,19 @@
 # THIRD-PARTY Libraries
 #
 include third-party/spdk.mk
+include third-party/liburing.mk
 
 .PHONY: third-party-clean
 third-party-clean:
 	@echo "## xNVMe: make third-party-clean"
 	$(MAKE) third-party-spdk-clean
+	$(MAKE) third-party-liburing-clean
 
 .PHONY: third-party-clobber
 third-party-clobber: third-party-clean
 	@echo "## xNVMe: make third-party-clobber"
 	$(MAKE) third-party-spdk-clobber
+	$(MAKE) third-party-liburing-clobber
 
 .PHONY: third-party-update
 third-party-update:
