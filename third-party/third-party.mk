807d24d93c13f3150653b76c7136b509b96c3d9e
Simon A. F. Lund <simon.lund@samsung.com>
2020-04-02 15:39:37 +0200

    3p:mk: including mk-file for 'be:spdk' dependency
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- third-party/third-party.mk
+++ third-party/third-party.mk
@@ -1,14 +1,17 @@
 #
 # THIRD-PARTY Libraries
 #
+include third-party/spdk.mk
 
 .PHONY: third-party-clean
 third-party-clean:
 	@echo "## xNVMe: make third-party-clean"
+	$(MAKE) third-party-spdk-clean
 
 .PHONY: third-party-clobber
 third-party-clobber: third-party-clean
 	@echo "## xNVMe: make third-party-clobber"
+	$(MAKE) third-party-spdk-clobber
 
 .PHONY: third-party-update
 third-party-update:
