5ffb7dfdc5cb30eaa537d4e74a83c26e90fe07d4
Simon A. F. Lund <simon.lund@samsung.com>
2020-04-15 16:29:24 +0200

    be:liou: removed superfluous function declarations
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- include/xnvme_be_liou.h
+++ include/xnvme_be_liou.h
@@ -36,33 +36,4 @@ XNVME_STATIC_ASSERT(
 	"Incorrect size"
 )
 
-int
-xnvme_be_liou_async_init(struct xnvme_dev *dev, struct xnvme_async_ctx **ctx,
-			 uint16_t depth, int flags);
-
-int
-xnvme_be_liou_async_term(struct xnvme_async_ctx *ctx);
-
-int
-xnvme_be_liou_async_poke(struct xnvme_async_ctx *ctx,
-			 uint32_t max);
-
-int
-xnvme_be_liou_async_wait(struct xnvme_async_ctx *ctx);
-
-void *
-xnvme_be_liou_buf_alloc(const struct xnvme_dev *dev, size_t nbytes,
-			uint64_t *phys);
-
-void
-xnvme_be_liou_buf_free(const struct xnvme_dev *XNVME_UNUSED(dev), void *buf);
-
-int
-xnvme_be_liou_cmd_pass(struct xnvme_dev *dev, struct xnvme_spec_cmd *cmd,
-		       void *dbuf, size_t dbuf_nbytes, void *mbuf,
-		       size_t mbuf_nbytes, int opts, struct xnvme_req *req);
-
-void
-xnvme_be_liou_dev_close(struct xnvme_dev *dev);
-
 #endif /* __INTERNAL_XNVME_BE_LIOU */
