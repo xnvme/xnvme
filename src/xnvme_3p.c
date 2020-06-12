bc7467456da6cab16a97fc41c9bcd5b39e379c5d
Simon A. F. Lund <simon.lund@samsung.com>
2020-06-12 10:46:49 +0200

    3p:ver: added printers of third-party version-strings
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ src/xnvme_3p.c
@@ -0,0 +1,64 @@
+// Copyright (C) Simon A. F. Lund <simon.lund@samsung.com>
+// SPDX-License-Identifier: Apache-2.0
+#include <libxnvme.h>
+
+#include "xnvme_3p/xnvme_3p_fio.c"
+#ifdef XNVME_BE_SPDK_ENABLED
+#include "xnvme_3p/xnvme_3p_spdk.c"
+#endif
+#ifdef XNVME_BE_LIOC_ENABLED
+#include "xnvme_3p/xnvme_3p_libnvme.c"
+#endif
+#ifdef XNVME_BE_LIOU_ENABLED
+#include "xnvme_3p/xnvme_3p_liburing.c"
+#endif
+
+const char *xnvme_3p_ver[] = {
+	xnvme_3p_fio,
+#ifdef XNVME_BE_SPDK_ENABLED
+	xnvme_3p_spdk,
+#endif
+#ifdef XNVME_BE_LIOC_ENABLED
+	xnvme_3p_libnvme,
+#endif
+#ifdef XNVME_BE_LIOU_ENABLED
+	xnvme_3p_liburing,
+#endif
+	0,
+};
+
+int
+xnvme_3p_ver_fpr(FILE *stream, const char *ver[], enum xnvme_pr opts)
+{
+	int wrtn = 0;
+
+	switch (opts) {
+	case XNVME_PR_TERSE:
+		wrtn += fprintf(stream, "# ENOSYS: opts(%x)", opts);
+		return wrtn;
+
+	case XNVME_PR_DEF:
+	case XNVME_PR_YAML:
+		break;
+	}
+
+	wrtn += fprintf(stream, "xnvme_3p:");
+
+	if ((!ver) || (!ver[0])) {
+		wrtn += fprintf(stream, " ~\n");
+		return wrtn;
+	}
+
+	wrtn += fprintf(stream, "\n");
+	for (int i = 0; xnvme_3p_ver[i]; ++i) {
+		fprintf(stream, "  - '%s'\n", xnvme_3p_ver[i]);
+	}
+
+	return wrtn;
+}
+
+int
+xnvme_3p_ver_pr(const char *ver[], enum xnvme_pr opts)
+{
+	return xnvme_3p_ver_fpr(stdout, ver, opts);
+}
