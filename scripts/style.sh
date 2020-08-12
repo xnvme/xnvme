cb2c810f358eaa0797ea60e414d6421709f033fb
Simon A. F. Lund <simon.lund@samsung.com>
2020-08-12 07:29:25 +0200

    scripts:style: added exit 1 when astyle formats
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- scripts/style.sh
+++ scripts/style.sh
@@ -12,6 +12,14 @@ if [[ ! -f "$ASTYLE_FNAME" ]]; then
   exit 1
 fi
 
+hash astyle 2>/dev/null || { echo >&2 "Please install astyle."; exit 1; }
+
 FILES=$(find ../{include,src,examples,tests,tools} -type f -name *.h -o -name *.c | grep -v xnvme_fioe.c)
 
-astyle --options=xnvme.astyle ${FILES}
+astyle --options=xnvme.astyle ${FILES} >> astyle.log
+if grep -iq "^formatted" astyle.log; then
+  echo "astyle did changes, please check"
+  exit 1
+fi
+
+exit 0
