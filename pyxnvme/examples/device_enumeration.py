a15dee3bcbdb29ac011ae9a0c104130e5c32123d
Simon A. F. Lund <simon.lund@samsung.com>
2020-09-02 22:36:08 +0200

    pyxnvme: style-fix
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- pyxnvme/examples/device_enumeration.py
+++ pyxnvme/examples/device_enumeration.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""
+    Showing how to enumerated devices via the ctypes-wrapped xNVMe C API
+"""
 from ctypes import byref, POINTER
-from xnvme import CAPI as capi
 import xnvme
 
 def main():
@@ -8,9 +10,9 @@ def main():
 
     arg = POINTER(xnvme.Enumeration)()
 
-    capi.xnvme_enumerate(byref(arg), 0x0)
+    xnvme.CAPI.xnvme_enumerate(byref(arg), 0x0)
 
-    capi.xnvme_enumeration_pr(arg, 0x0)
+    xnvme.CAPI.xnvme_enumeration_pr(arg, 0x0)
 
 if __name__ == "__main__":
     main()
