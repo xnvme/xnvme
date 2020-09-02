bdf6070e74fd8f891a92e227ca6fc1a90ea9d9ec
Simon A. F. Lund <simon.lund@samsung.com>
2020-09-02 21:25:31 +0200

    scripts:pkgs:buster: changed to use backports instead of pip
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- scripts/pkgs/debian:buster.sh
+++ scripts/pkgs/debian:buster.sh
@@ -8,6 +8,8 @@ ldd --version || true
 # Unattended update and upgrade
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
+# Add buster-backports for meson and ninja
+echo "deb http://deb.debian.org/debian buster-backports main contrib non-free" > /etc/apt/sources.list.d/buster-backports.list
 apt-get -qy update
 apt-get -qy \
   -o "Dpkg::Options::=--force-confdef" \
@@ -17,5 +19,4 @@ apt-get -qy autoclean
 # Install packages via apt-get
 apt-get install -qy $(cat "scripts/pkgs/debian:buster.txt")
 
-# Install packages via PyPI
-pip3 install meson ninja
+apt-get -t buster-backports install -qy meson ninja-build
