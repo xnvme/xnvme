2c6b2b040c12aed05d22770b3363d78153bfb00b
Simon A. F. Lund <simon.lund@samsung.com>
2020-12-02 17:42:53 +0100

    api:lba: add helpers for constructing lba-ranges
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ include/libxnvme_lba.h
@@ -0,0 +1,112 @@
+/**
+ * Utilities to tedious tasks with LBAs, such as generating and validating ranges of them
+ *
+ * Copyright (C) Simon A. F. Lund <simon.lund@samsung.com>
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * @file libxnvme_lba.h
+ */
+#ifndef __LIBXNVME_LBA_H
+#define __LIBXNVME_LBA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Representation of a range of logical-block-addresses aka LBAs
+ *
+ * @struct xnvme_lba_range
+ */
+struct xnvme_lba_range {
+	uint64_t slba;		///< Range start-LBA; begins at and includes this address
+	uint64_t elba;		///< Range end-LBA; ends at and includes this address
+	uint32_t naddrs;	///< Number of addresses in range [slba, elba]
+	uint64_t nbytes;	///< Number of bytes covered by [slba, elba]
+
+	struct {
+		uint32_t is_zoned : 1; ///< Whether the range covers a zone [zslba, zslba + cap]
+		uint32_t is_valid : 1; ///< Whether the range is valid
+		uint32_t rsvd : 30;
+	} attr;
+};
+
+/**
+ * Prints the given backend attribute to the given output stream
+ *
+ * @param stream output stream used for printing
+ * @param range Pointer to the ::xnvme_lba_range to print
+ * @param opts printer options, see ::xnvme_pr
+ *
+ * @return On success, the number of characters printed is returned.
+ */
+int
+xnvme_lba_range_fpr(FILE *stream, struct xnvme_lba_range *range, int opts);
+
+/**
+ * Prints the given backend attribute to stdout
+ *
+ * @param range Pointer to the ::xnvme_lba_range to print
+ * @param opts printer options, see ::xnvme_pr
+ *
+ * @return On success, the number of characters printed is returned.
+ */
+int
+xnvme_lba_range_pr(struct xnvme_lba_range *range, int opts);
+
+/**
+ * Initializes a range starting at the given byte-offset and spanning nbytes
+ *
+ * @param dev Device handle obtained with xnvme_dev_open() / xnvme_dev_openf()
+ * @param offset Offset in bytes satisfying offset < capacity
+ * @param nbytes Number of bytes, satisfying offset + nbytes < capacity
+ *
+ * @return On success, an initialized range is returned by value with attr.is_valid = 1. On error,
+ * a potential non, or partially, initialized struct is returned by value with attr.is_valid = 0.
+ */
+struct xnvme_lba_range
+xnvme_lba_range_from_offset_nbytes(struct xnvme_dev *dev, uint64_t offset, uint64_t nbytes);
+
+/**
+ * Initializes a range starting at the given start-lba (slba) and spanning naddrs
+ *
+ * @param dev Device handle obtained with xnvme_dev_open() / xnvme_dev_openf()
+ * @param slba A valid lba on the given dev
+ * @param elba A valid lba on the given dev
+ *
+ * @return On success, an initialized range is returned by value with attr.is_valid = 1. On error,
+ * a potential non, or partially, initialized struct is returned by value with attr.is_valid = 0.
+ */
+struct xnvme_lba_range
+xnvme_lba_range_from_slba_elba(struct xnvme_dev *dev, uint64_t slba, uint64_t elba);
+
+/**
+ * Initializes a range starting at the given start-lba (slba) and spanning naddrs
+ *
+ * @param dev Device handle obtained with xnvme_dev_open() / xnvme_dev_openf()
+ * @param slba A valid lba on the given dev
+ * @param naddrs A non-zero number of addresses, satisfying slba + naddrs < device capacity
+ *
+ * @return On success, an initialized range is returned by value with attr.is_valid = 1. On error,
+ * a potential non, or partially, initialized struct is returned by value with attr.is_valid = 0.
+ */
+struct xnvme_lba_range
+xnvme_lba_range_from_slba_naddrs(struct xnvme_dev *dev, uint64_t slba, uint64_t naddrs);
+
+/**
+ * Initializes a range starting at the given zone-start-LBA and spanning zone-capacity
+ *
+ * @param dev Device handle obtained with xnvme_dev_open() / xnvme_dev_openf()
+ * @param zdescr Pointer to a zone-descriptor valid on the given dev
+ *
+ * @return On success, an initialized range is returned by value with attr.is_valid = 1. On error,
+ * a potential non, or partially, initialized struct is returned by value with attr.is_valid = 0.
+ */
+struct xnvme_lba_range
+xnvme_lba_range_from_zdescr(struct xnvme_dev *dev, struct xnvme_spec_znd_descr *zdescr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __LIBXNVME_H */
---
+++ src/xnvme_lba.c
@@ -0,0 +1,104 @@
+// Copyright (C) Simon A. F. Lund <simon.lund@samsung.com>
+// SPDX-License-Identifier: Apache-2.0
+#include <libxnvme.h>
+#include <libxnvme_lba.h>
+#include <libxnvme_pp.h>
+
+int
+xnvme_lba_range_fpr(FILE *stream, struct xnvme_lba_range *range, int opts)
+{
+	int wrtn = 0;
+
+	switch (opts) {
+	case XNVME_PR_TERSE:
+		wrtn += fprintf(stream, "# ENOSYS: opts(%x)", opts);
+		return wrtn;
+
+	case XNVME_PR_DEF:
+	case XNVME_PR_YAML:
+		break;
+	}
+
+	wrtn += fprintf(stream, "xnvme_lba_range:");
+	if (!range) {
+		wrtn += fprintf(stream, "~\n");
+		return wrtn;
+	}
+
+	wrtn += fprintf(stream, "\n");
+
+	wrtn += fprintf(stream, "  slba: 0x%016lx\n", range->slba);
+	wrtn += fprintf(stream, "  elba: 0x%016lx\n", range->elba);
+	wrtn += fprintf(stream, "  naddrs: %u\n", range->naddrs);
+	wrtn += fprintf(stream, "  nbytes: %zu\n", range->nbytes);
+	wrtn += fprintf(stream, "  attr: { is_zones: %d, is_valid: %d}\n", range->attr.is_zoned,
+			range->attr.is_valid);
+
+	return wrtn;
+}
+
+int
+xnvme_lba_range_pr(struct xnvme_lba_range *range, int opts)
+{
+	return xnvme_lba_range_fpr(stdout, range, opts);
+}
+
+struct xnvme_lba_range
+xnvme_lba_range_from_slba_naddrs(struct xnvme_dev *dev, uint64_t slba, uint64_t naddrs)
+{
+	const struct xnvme_geo *geo = xnvme_dev_get_geo(dev);
+	struct xnvme_lba_range rng = { 0 };
+
+	if (!naddrs) {
+		XNVME_DEBUG("FAILED: !naddrs => the range must be non-empty");
+		return rng;
+	}
+
+	rng.slba = slba;
+	rng.elba = slba + naddrs - 1;
+	rng.naddrs = naddrs;
+	rng.nbytes = naddrs * geo->nbytes;
+	rng.attr.is_valid = 1;
+
+	return rng;
+}
+
+struct xnvme_lba_range
+xnvme_lba_range_from_slba_elba(struct xnvme_dev *dev, uint64_t slba, uint64_t elba)
+{
+	struct xnvme_lba_range rng = { 0 };
+
+	if (slba > elba) {
+		XNVME_DEBUG("FAILED: invalid range; slba > elba");
+		return rng;
+	}
+
+	return xnvme_lba_range_from_slba_naddrs(dev, slba, (elba - slba) + 1);
+}
+
+struct xnvme_lba_range
+xnvme_lba_range_from_offset_nbytes(struct xnvme_dev *dev, uint64_t offset, uint64_t nbytes)
+{
+	const struct xnvme_geo *geo = xnvme_dev_get_geo(dev);
+	struct xnvme_lba_range rng = { 0 };
+
+	if (offset % geo->nbytes) {
+		XNVME_DEBUG("FAILED: offset: %zu, does not align to lba-width: %u", offset,
+			    geo->nbytes);
+		return rng;
+	}
+	if (nbytes % geo->nbytes) {
+		XNVME_DEBUG("FAILED: nbytes: %zu, does not align to lba-width: %u", nbytes,
+			    geo->nbytes);
+		return rng;
+	}
+
+	return xnvme_lba_range_from_slba_naddrs(dev, offset / geo->nbytes, nbytes / geo->nbytes);
+}
+
+struct xnvme_lba_range
+xnvme_lba_range_from_zdescr(struct xnvme_dev *dev, struct xnvme_spec_znd_descr *zdescr)
+{
+	return xnvme_lba_range_from_slba_naddrs(dev, zdescr->zslba, zdescr->zcap);
+}
+
