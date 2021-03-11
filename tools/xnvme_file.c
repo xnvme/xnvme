a3adcdfb3f4f12c5e16b6507123505d7ff522def
Simon A. F. Lund <simon.lund@samsung.com>
2021-03-11 10:26:44 +0100

    tools:file: add sub-command 'copy-sync'
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- tools/xnvme_file.c
+++ tools/xnvme_file.c
@@ -390,6 +390,79 @@ exit:
 	return 0;
 }
 
+int
+copy_file_sync(struct xnvmec *cli)
+{
+	const char *src_fpath, *dst_fpath;
+	struct xnvme_dev *src_fh, *dst_fh;
+	int src_flags, dst_flags;
+	size_t buf_nbytes, tbytes, iosize;
+	char *buf = NULL;
+
+	src_fpath = cli->args.data_input;
+	dst_fpath = cli->args.data_output;
+	src_flags = XNVME_FILE_OFLG_RDONLY;
+	src_flags |= cli->given[XNVMEC_OPT_DIRECT] ? XNVME_FILE_OFLG_DIRECT_ON : 0x0;
+	dst_flags = XNVME_FILE_OFLG_CREATE | XNVME_FILE_OFLG_WRONLY;
+	dst_flags |= cli->given[XNVMEC_OPT_DIRECT] ? XNVME_FILE_OFLG_DIRECT_ON : 0x0;
+	iosize = cli->given[XNVMEC_OPT_IOSIZE] ? cli->args.iosize : IOSIZE_DEF;
+
+	src_fh = xnvme_file_open(src_fpath, src_flags);
+	if (src_fh == NULL) {
+		xnvmec_perr("xnvme_file_open(src)", errno);
+		return errno;
+	}
+	dst_fh = xnvme_file_open(dst_fpath, dst_flags);
+	if (dst_fh == NULL) {
+		xnvmec_perr("xnvme_file_open(dst)", errno);
+		goto exit;
+	}
+	tbytes = xnvme_dev_get_geo(src_fh)->tbytes;
+
+	buf_nbytes = iosize;
+	buf = xnvme_buf_alloc(src_fh, buf_nbytes);
+	if (!buf) {
+		xnvmec_perr("xnvme_buf_alloc()", errno);
+		goto exit;
+	}
+	xnvmec_buf_fill(buf, buf_nbytes, "zero");
+
+	xnvmec_pinf("copy-sync: {src: %s, dst: %s, tbytes: %zu, buf_nbytes: %zu, iosize: %zu",
+		    src_fpath, dst_fpath, tbytes, buf_nbytes, iosize);
+
+	xnvmec_timer_start(cli);
+
+	for (size_t ofz = 0; ofz < tbytes; ofz += iosize) {
+		struct xnvme_cmd_ctx src_ctx = xnvme_file_get_cmd_ctx(src_fh);
+		struct xnvme_cmd_ctx dst_ctx = xnvme_file_get_cmd_ctx(dst_fh);
+		size_t nbytes = XNVME_MIN_U64(iosize, tbytes - ofz);
+		ssize_t res;
+
+		res = xnvme_file_pread(&src_ctx, buf, nbytes, ofz);
+		if (res || xnvme_cmd_ctx_cpl_status(&src_ctx)) {
+			xnvmec_perr("xnvme_file_pread(src)", res);
+			xnvme_cmd_ctx_pr(&src_ctx, XNVME_PR_DEF);
+			goto exit;
+		}
+
+		res = xnvme_file_pwrite(&dst_ctx, buf, nbytes, ofz);
+		if (res || xnvme_cmd_ctx_cpl_status(&dst_ctx)) {
+			xnvmec_perr("xnvme_file_pwrite(dst)", res);
+			xnvme_cmd_ctx_pr(&dst_ctx, XNVME_PR_DEF);
+			goto exit;
+		}
+	}
+
+	xnvmec_timer_stop(cli);
+	xnvmec_timer_bw_pr(cli, "wall-clock", tbytes);
+
+exit:
+	xnvme_buf_free(src_fh, buf);
+	xnvme_file_close(src_fh);
+	xnvme_file_close(dst_fh);
+	return 0;
+}
+
 static struct xnvmec_sub g_subs[] = {
 	{
 		"write-read", "Write and read a file",
@@ -431,6 +504,15 @@ static struct xnvmec_sub g_subs[] = {
 			{XNVMEC_OPT_DATA_INPUT, XNVMEC_POSA},
 			{XNVMEC_OPT_IOSIZE, XNVMEC_LOPT},
 			{XNVMEC_OPT_QDEPTH, XNVMEC_LOPT},
+			{XNVMEC_OPT_DIRECT, XNVMEC_LFLG},
+		}
+	},
+	{
+		"copy-sync", "Copy file --data-input to --data--output",
+		"Copy file --data-input to --data--output", copy_file_sync, {
+			{XNVMEC_OPT_DATA_INPUT, XNVMEC_POSA},
+			{XNVMEC_OPT_DATA_OUTPUT, XNVMEC_POSA},
+			{XNVMEC_OPT_IOSIZE, XNVMEC_LOPT},
 			{XNVMEC_OPT_DIRECT, XNVMEC_LFLG},
 		}
 	},
