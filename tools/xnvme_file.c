f39b5f45b6d105cefa27bce629d944d34494eb0e
Simon A. F. Lund <simon.lund@samsung.com>
2021-03-11 10:22:09 +0100

    tools:file: add sub-command 'dump-sync'
    Allocates a buffer of '--data-nbytes' and writes it to the file '--data-output'.
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- tools/xnvme_file.c
+++ tools/xnvme_file.c
@@ -7,6 +7,7 @@
 #include <libxnvmec.h>
 #include <libxnvme_file.h>
 
+#define IOSIZE_DEF 4096
 
 int
 read_write(struct xnvmec *cli)
@@ -55,12 +56,77 @@ exit:
 	return 0;
 }
 
+int
+dump_sync(struct xnvmec *cli)
+{
+	const char *fpath;
+	struct xnvme_dev *fh;
+	int flags;
+	size_t buf_nbytes, tbytes, iosize;
+	char *buf;
+
+	fpath = cli->args.data_output;
+	flags = XNVME_FILE_OFLG_CREATE | XNVME_FILE_OFLG_WRONLY;
+	flags |= cli->given[XNVMEC_OPT_DIRECT] ? XNVME_FILE_OFLG_DIRECT_ON : 0x0;
+	iosize = cli->given[XNVMEC_OPT_IOSIZE] ? cli->args.iosize : IOSIZE_DEF;
+
+	fh = xnvme_file_open(fpath, flags);
+	if (!fh) {
+		xnvmec_perr("xnvme_file_open(fh)", errno);
+		return errno;
+	}
+	tbytes = cli->args.data_nbytes;
+
+	buf_nbytes = tbytes;
+	buf = xnvme_buf_alloc(fh, buf_nbytes);
+	if (!buf) {
+		xnvmec_perr("xnvme_buf_alloc()", errno);
+		goto exit;
+	}
+	xnvmec_buf_fill(buf, buf_nbytes, "anum");
+
+	xnvmec_pinf("dump-sync: {fpath: %s, tbytes: %zu, buf_nbytes: %zu iosize: %zu}",
+		    fpath, tbytes, buf_nbytes, iosize);
+
+	xnvmec_timer_start(cli);
+
+	for (size_t ofz = 0; ofz < tbytes; ofz += iosize) {
+		struct xnvme_cmd_ctx ctx = xnvme_file_get_cmd_ctx(fh);
+		size_t nbytes = XNVME_MIN_U64(iosize, tbytes - ofz);
+		ssize_t res;
+
+		res = xnvme_file_pwrite(&ctx, buf + ofz, nbytes, ofz);
+		if (res || xnvme_cmd_ctx_cpl_status(&ctx)) {
+			xnvmec_perr("xnvme_file_pwrite(fh)", res);
+			xnvme_cmd_ctx_pr(&ctx, XNVME_PR_DEF);
+			goto exit;
+		}
+	}
+
+	xnvmec_timer_stop(cli);
+	xnvmec_timer_bw_pr(cli, "wall-clock", tbytes);
+
+exit:
+	xnvme_buf_free(fh, buf);
+	xnvme_file_close(fh);
+	return 0;
+}
+
 static struct xnvmec_sub g_subs[] = {
 	{
 		"write-read", "Write and read a file",
 		"Write and read a file", read_write, {
 			{XNVMEC_OPT_SYS_URI, XNVMEC_POSA},
 			{XNVMEC_OPT_DATA_NBYTES, XNVMEC_POSA},
+		}
+	},
+	{
+		"dump-sync", "Write a buffer of 'data-nbytes' to file",
+		"Write a buffer of 'data-nbytes' to file", dump_sync, {
+			{XNVMEC_OPT_DATA_OUTPUT, XNVMEC_POSA},
+			{XNVMEC_OPT_DATA_NBYTES, XNVMEC_LREQ},
+			{XNVMEC_OPT_IOSIZE, XNVMEC_LOPT},
+			{XNVMEC_OPT_DIRECT, XNVMEC_LFLG},
 		}
 	},
 };
