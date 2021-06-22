b9531e41fabc6e9f410fd8816dd865b22b0c2a60
Simon A. F. Lund <simon.lund@samsung.com>
2021-06-22 23:08:57 +0200

    scripts:pkgs: add FreeBSD 13 packages
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ scripts/pkgs/freebsd-13.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Query the linker version
+ld --version || true
+
+# Query the (g)libc version
+ldd --version || true
+
+# Install packages via pkg
+pkg install -qy $(cat "scripts/pkgs/freebsd-12.txt")
+
+# Install packages via PyPI
+pip3 install meson ninja
