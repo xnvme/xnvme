881586c4c086d8e2786d3ff1ce5df5df7a2382e9
Simon A. F. Lund <simon.lund@samsung.com>
2022-02-04 10:53:32 +0100

    scripts:pkgs: update packages for FreeBSD 13 and add install-notes
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- scripts/pkgs/freebsd-13.sh
+++ scripts/pkgs/freebsd-13.sh
@@ -7,3 +7,9 @@ ldd --version || true
 
 # Install packages via pkg
 pkg install -qy $(cat "scripts/pkgs/freebsd-13.txt")
+
+# Make sure you have the kernel source, the headers are needed
+#wget -O /usr/src/src.txz https://download.freebsd.org/ftp/releases/amd64/13.0-RELEASE/src.txz
+#tar -C /usr/src/ -xf /usr/src/src.txz
+#git clone --branch releng/13.0 https://git.FreeBSD.org/src.git --single-branch --depth=1 /usr/src
+#svnlite checkout https://svn.freebsd.org/base/releng/13.0/sys/ /usr/src/sys
