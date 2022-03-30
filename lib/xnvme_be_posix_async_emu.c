31f624505128001c598ec8ecefdfb7d33e1af4d7
Simon A. F. Lund <simon.lund@samsung.com>
2022-03-30 15:04:53 +0200

    be:async:emu: add async. iov() aka support for Vectored I/O
    With support for NVMe-iovec-ioctl(), as well as regular iovec-support,
    then the emulated asynchronous interface needs to forward this to the
    synchronous implementation. This changes does so.
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- lib/xnvme_be_posix_async_emu.c
+++ lib/xnvme_be_posix_async_emu.c
@@ -10,13 +10,22 @@
 #include <xnvme_queue.h>
 #include <xnvme_dev.h>
 
+/**
+ * NOTE: this should be possible to do within a single cache-line... refactor pointers for re-use
+ *       and use a flag to distinguish contig vs. iovec payload
+ */
 struct _emu_entry {
 	struct xnvme_dev *dev;
 	struct xnvme_cmd_ctx *ctx;
-	void *dbuf;
-	size_t dbuf_nbytes;
-	void *mbuf;
-	size_t mbuf_nbytes;
+
+	void *data;
+	void *meta;
+	uint32_t data_nbytes;
+	uint32_t data_vec_cnt;
+	uint32_t meta_nbytes;
+	uint32_t meta_vec_cnt;
+	uint32_t is_vectored;
+
 	STAILQ_ENTRY(_emu_entry) link;
 };
 
@@ -112,8 +121,14 @@ _posix_async_emu_poke(struct xnvme_queue *q, uint32_t max)
 
 		STAILQ_REMOVE_HEAD(&qp->sq, link);
 
-		err = queue->base.dev->be.sync.cmd_io(entry->ctx, entry->dbuf, entry->dbuf_nbytes,
-						      entry->mbuf, entry->mbuf_nbytes);
+		err = entry->is_vectored
+			      ? queue->base.dev->be.sync.cmd_iov(
+					entry->ctx, entry->data, entry->data_vec_cnt,
+					entry->data_nbytes, entry->meta, entry->meta_vec_cnt,
+					entry->meta_nbytes)
+			      : queue->base.dev->be.sync.cmd_io(entry->ctx, entry->data,
+								entry->data_nbytes, entry->meta,
+								entry->meta_nbytes);
 		if (err) {
 			XNVME_DEBUG("FAILED: err: %d", err);
 		}
@@ -147,10 +162,14 @@ _posix_async_emu_cmd_io(struct xnvme_cmd_ctx *ctx, void *dbuf, size_t dbuf_nbyte
 
 	entry->dev = ctx->dev;
 	entry->ctx = ctx;
-	entry->dbuf = dbuf;
-	entry->dbuf_nbytes = dbuf_nbytes;
-	entry->mbuf = mbuf;
-	entry->mbuf_nbytes = mbuf_nbytes;
+
+	entry->data = dbuf;
+	entry->data_nbytes = dbuf_nbytes;
+	entry->data_vec_cnt = 0;
+	entry->meta = mbuf;
+	entry->meta_nbytes = mbuf_nbytes;
+	entry->meta_vec_cnt = 0;
+	entry->is_vectored = false;
 
 	STAILQ_INSERT_TAIL(&qp->sq, entry, link);
 
@@ -158,13 +177,49 @@ _posix_async_emu_cmd_io(struct xnvme_cmd_ctx *ctx, void *dbuf, size_t dbuf_nbyte
 
 	return 0;
 }
+
+static inline int
+_posix_async_emu_cmd_iov(struct xnvme_cmd_ctx *ctx, struct iovec *dvec, size_t dvec_cnt,
+			 size_t dvec_nbytes, struct iovec *mvec, size_t mvec_cnt,
+			 size_t mvec_nbytes)
+{
+	struct xnvme_queue_emu *queue = (void *)ctx->async.queue;
+	struct _emu_qp *qp = queue->qp;
+	struct _emu_entry *entry;
+
+	// Grab entry from rp and push into sq
+	entry = STAILQ_FIRST(&qp->rp);
+	if (!entry) {
+		XNVME_DEBUG("FAILED: should not happen");
+		return -EIO;
+	}
+	STAILQ_REMOVE_HEAD(&qp->rp, link);
+
+	entry->dev = ctx->dev;
+	entry->ctx = ctx;
+
+	entry->data = dvec;
+	entry->data_nbytes = dvec_nbytes;
+	entry->data_vec_cnt = dvec_cnt;
+	entry->meta = mvec;
+	entry->meta_nbytes = mvec_nbytes;
+	entry->meta_vec_cnt = mvec_cnt;
+	entry->is_vectored = true;
+
+	STAILQ_INSERT_TAIL(&qp->sq, entry, link);
+
+	ctx->async.queue->base.outstanding += 1;
+
+	return 0;
+}
+
 #endif
 
 struct xnvme_be_async g_xnvme_be_posix_async_emu = {
 	.id = "emu",
 #ifdef XNVME_BE_ASYNC_EMU_ENABLED
 	.cmd_io = _posix_async_emu_cmd_io,
-	.cmd_iov = xnvme_be_nosys_queue_cmd_iov,
+	.cmd_iov = _posix_async_emu_cmd_iov,
 	.poke = _posix_async_emu_poke,
 	.wait = xnvme_be_nosys_queue_wait,
 	.init = _posix_async_emu_init,
