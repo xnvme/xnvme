43cc0bdfbc709c476a43324fdd06b81963bc8dda
Simon A. F. Lund <simon.lund@samsung.com>
2022-04-21 13:30:23 +0200

    scripts: change macos install-script due to brew-breakage
    On MacOS with git is installed, but not installed by brew. Then 'brew
    install git' fails for git 2.35.1. To work-around this, then the
    install-script is changed to check whether the various tools are
    installed and only install them via brew if they are not.
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- scripts/pkgs/macos-11.sh
+++ scripts/pkgs/macos-11.sh
@@ -2,7 +2,10 @@
 
 # Install packages via brew
 brew update
-brew install $(cat "scripts/pkgs/macos-11.txt")
+brew upgrade
+clang-format --version && echo "Installed" || brew install clang-format
+git --version && echo "Installed" || brew install git
+python3 --version && echo "Installed" || brew install python3
 
 # Install packages via PyPI
 pip3 install meson ninja
