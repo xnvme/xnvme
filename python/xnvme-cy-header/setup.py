3cf067738fe2d5e66f4b8d397cf113fbdca30e1e
Mads Ynddal <m.ynddal@samsung.com>
2022-06-23 11:28:32 +0200

    py: add Cython header (libxnvme.pxd) for the xNVMe C API
    This is the first attempt at auto-generating Cython-header (pxd files)
    for xNVMe. This enables us to develop scripts in Cython, which can be
    used for enhanced testing frameworks like pytest, third-party
    applications or a feature set of Python bindings.
    
    pytests for xNVMe Cython header
    -------------------------------
    
    Added a proof-of-concept set of pytests for the xNVMe Cython header.
    These only test the most basic functionality, but is a working example
    of Cython/xNVMe interfacing.
    
    It's required to build the .pyx file before running pytest. This done
    using the setup.py file in the tests directory.
    
    Signed-off-by: Mads Ynddal <m.ynddal@samsung.com>

---
+++ python/xnvme-cy-header/setup.py
@@ -0,0 +1,41 @@
+import glob
+
+from setuptools import find_namespace_packages, setup
+
+try:
+    with open("README.rst", "r") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+setup(
+    name="xnvme-cy-header",
+    version="0.3.0",
+    author="Mads Ynddal",
+    author_email="m.ynddal@samsung.com",
+    description="Cython header (libxnvme.pxd) for the xNVMe C API",
+    long_description=long_description,
+    url="https://github.com/OpenMPDK/xNVMe",
+    project_urls={
+        "Bug Tracker": "https://github.com/OpenMPDK/xNVMe/issues",
+    },
+    license="Apache License, Version 2.0",
+    classifiers=[
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    data_files=[
+        ("bin", glob.glob("bin/*")),
+    ],
+    packages=find_namespace_packages(include=["xnvme.*"]),
+    package_data={"": ["libxnvme.pxd", "test_cython.pyx", "requirements.txt"]},
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=[
+        "pytest",
+        "setuptools>=40.1",  # Introduction of find_namespace_packages
+    ],
+    zip_safe=False,
+)
