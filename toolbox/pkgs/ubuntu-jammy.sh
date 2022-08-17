27dbd6e677c45c1d190c86d76f8db79f6158419d
Karl Bonde Torp <k.torp@samsung.com>
2022-08-17 13:08:30 +0200

    ci,docs: add Ubuntu Jammy Add the latest LTS release of Ubuntu
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

---
+++ toolbox/pkgs/ubuntu-jammy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Query the linker version
+ld --version || true
+
+# Query the (g)libc version
+ldd --version || true
+
+# Unattended update and upgrade
+export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_PRIORITY=critical
+apt-get -qy update
+apt-get -qy \
+        -o "Dpkg::Options::=--force-confdef" \
+        -o "Dpkg::Options::=--force-confold" upgrade
+apt-get -qy autoclean
+
+# Install packages via the system package-manager (apt-get)
+apt-get install -qy $(cat "toolbox/pkgs/ubuntu-jammy.txt")
+
+# Install packages via the Python package-manager (pip)
+python3 -m pip install meson ninja pyelftools
