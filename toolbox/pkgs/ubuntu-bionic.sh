fca316f25260753a49f77f24c870900f43f3b8a5
Simon A. F. Lund <simon.lund@samsung.com>
2022-09-21 10:44:43 +0200

    3p:spdk: prep for mandatory isa-l
    In the next SPDK release 'isa-l' will be "mandatory", e.g. not possible
    to disable it via '--without-isal'. To prepare for this, then the build
    of the SPDK subproject removes the '--without-isal', that is, enables
    the use of libisal. Additionally:
    
    * The meson-wrapping for SPDK is adjusted to link with libisal
    * The 'pkgs' install scripts are extended with tools requires by isa-l
      - Arch Linux: add nasm
      - CentOS 7: add autoconf, libtool
      - CentOS Stream: add autoconf, libtool, and nasm
      - Debian: add autoconf and libtool (nasm was already there)
      - Fedora: add autoconf, libtool, and nasm
      - FreeBSD: add libtool
      - OpenSUSE: add autoconf, libtool, and nasm
      - Ubuntu: add autoconf and libtool (nasm was already there)
    
    * Add build of nasm from source on distros with versions < 2.14
      - CentOS 7
      - Ubuntu Bionic
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- toolbox/pkgs/ubuntu-bionic.sh
+++ toolbox/pkgs/ubuntu-bionic.sh
@@ -17,6 +17,16 @@ apt-get -qy autoclean
 # Install packages via the system package-manager (apt-get)
 apt-get install -qy $(cat "toolbox/pkgs/ubuntu-bionic.txt")
 
+# Install nasm from source
+git clone https://github.com/netwide-assembler/nasm.git
+cd nasm
+git checkout nasm-2.15
+./autogen.sh
+./configure --prefix=/usr
+make -j $(nproc)
+make install
+cd ..
+
 # Install packages via the Python package-manager (pip)
 python3 -m pip install --upgrade pip
 python3 -m pip install meson ninja pyelftools
