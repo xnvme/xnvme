d57d1a921a904928be328d72d0e1c4fcef7a2312
Simon A. F. Lund <simon.lund@samsung.com>
2022-09-25 11:36:46 +0200

    be:linux:libaio: add spin-while-waiting for opts.poll_io
    The libaio backend implementation conservatively waits for at least a
    single I/O to complete and a timespec to put an upper bound on how long
    it waits. Such usage of the Linux aio interface is common as it
    conserves CPU utilization.
    
    However, one can trade CPU utilization for reduced latency and increased
    IO rate by repeatedly calling io_getevents() with a min-value of 0. This
    commit enables such behavior via the "poll_io" option for the libaio
    backend implementation.
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- include/xnvme_be_linux_libaio.h
+++ include/xnvme_be_linux_libaio.h
@@ -11,7 +11,8 @@ struct xnvme_queue_libaio {
 	io_context_t aio_ctx;
 	struct io_event *aio_events;
 
-	uint8_t rsvd[213];
+	uint8_t poll_io;
+	uint8_t rsvd[212];
 };
 XNVME_STATIC_ASSERT(sizeof(struct xnvme_queue_libaio) == XNVME_BE_QUEUE_STATE_NBYTES,
 		    "Incorrect size")
