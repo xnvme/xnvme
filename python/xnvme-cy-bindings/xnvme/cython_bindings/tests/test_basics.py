70d769af6c82df45ce41aff5fb6302ac0c2f4f4d
Mads Ynddal <m.ynddal@samsung.com>
2022-10-24 15:34:43 +0200

    py: fix void_pointer casting
    Fix issue with casting between encapsulated xNVMe objects in Python.
    Cython wouldn't do the proper casting, and cast a PyObject* instead of
    the integer value it represents.
    
    Signed-off-by: Mads Ynddal <m.ynddal@samsung.com>

--- python/xnvme-cy-bindings/xnvme/cython_bindings/tests/test_basics.py
+++ python/xnvme-cy-bindings/xnvme/cython_bindings/tests/test_basics.py
@@ -34,3 +34,20 @@ def test_ident(dev):
     ident = xnvme.xnvme_dev_get_ident(dev)
     assert ident.nsid == 1
     assert ident.to_dict()
+
+
+def test_pointers(dev):
+    # A random xNVMe object which is easy to create
+    ctx = xnvme.xnvme_cmd_ctx_from_dev(dev)
+
+    # Verify that we are pointing to the struct cmd embedded at the origin of ctx (they will share address)
+    assert ctx.void_pointer == ctx.cmd.void_pointer
+
+    # Verify that we are pointing to the struct cpl embedded at the origin of ctx + the size of cmd
+    assert ctx.void_pointer + ctx.cmd.sizeof == ctx.cpl.void_pointer
+
+    # Check pointers are well defined and stable -- i.e. not generated at each call
+    assert ctx.dev.void_pointer == ctx.dev.void_pointer
+
+    # Check that a pointer from two places to the same struct shows the same address
+    assert ctx.dev.void_pointer == xnvme.xnvme_dev_get_geo(ctx.dev).void_pointer
