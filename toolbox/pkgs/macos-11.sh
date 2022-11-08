6a4db03fcd4fbc604feb09386c17f40cfa2d0022
Simon A. F. Lund <os@safl.dk>
2022-11-08 15:02:20 +0100

    toolbox/pkgs/mac: remove update/upgrade of brew
    Due to issues installing meson via brew, then the update/upgrade is
    removed.
    
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- toolbox/pkgs/macos-11.sh
+++ toolbox/pkgs/macos-11.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# Install packages via brew
-brew update
-brew upgrade
+# Install packages via brew, assuming that brew is: installed, updated, and upgraded
 clang-format --version && echo "Installed" || brew install clang-format
 git --version && echo "Installed" || brew install git
 python3 --version && echo "Installed" || brew install python3
