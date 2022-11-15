ad78466c4fb3ed54177fcdb295e641024e11c5e4
Simon A. F. Lund <simon.lund@samsung.com>
2022-11-15 17:32:28 +0100

    be/cbi/psync: add cmd_iov() and normalize error-handling
    Part of the motivation for refactoring from POSIX to CBI is to let go of
    the pedantic POSIX requirement. That is, the implementations under the
    POSIX backend may not utilize anything defined outside of POSIX, as that
    would break POSIX compliance.
    
    One example of that is the use of pwritev()/preadv(), these are commonly
    available but not defined by POSIX. However, the Linux, FreeBSD, and
    macOS backends can all benefit from a common implementation.
    
    With this, a common psync-implementation with contigious buffers as well
    as iovecs are provided.
    
    Additionally, the error-handling is normalized, ensuring that
    completion-status is always filled out. This is especially useful when
    using psync via async-emulation where the user will assume the abilityto
    do completion-error-detection by inspecting the completion-status.
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- lib/xnvme_be_cbi_sync_psync.c
+++ lib/xnvme_be_cbi_sync_psync.c
@@ -1,7 +1,10 @@
 // Copyright (C) Simon A. F. Lund <simon.lund@samsung.com>
 // SPDX-License-Identifier: Apache-2.0
-#ifndef _XOPEN_SOURCE
-#define _XOPEN_SOURCE 700
+#ifndef _BSD_SOURCE
+#define _BSD_SOURCE
+#endif
+#ifndef _DEFAULT_SOURCE
+#define _DEFAULT_SOURCE
 #endif
 #include <xnvme_be.h>
 #include <xnvme_be_nosys.h>
@@ -19,42 +22,100 @@ xnvme_be_cbi_sync_psync_cmd_io(struct xnvme_cmd_ctx *ctx, void *dbuf, size_t dbu
 	struct xnvme_be_cbi_state *state = (void *)ctx->dev->be.state;
 	const uint64_t ssw = ctx->dev->geo.ssw;
 	ssize_t res;
+	uint8_t sc;
 
-	///< NOTE: opcode-dispatch (io)
 	switch (ctx->cmd.common.opcode) {
 	case XNVME_SPEC_NVM_OPC_WRITE:
 		res = pwrite(state->fd, dbuf, dbuf_nbytes, ctx->cmd.nvm.slba << ssw);
+		sc = res != (ssize_t)dbuf_nbytes ? errno : 0;
 		break;
-
 	case XNVME_SPEC_NVM_OPC_READ:
 		res = pread(state->fd, dbuf, dbuf_nbytes, ctx->cmd.nvm.slba << ssw);
+		sc = res != (ssize_t)dbuf_nbytes ? errno : 0;
 		break;
-
 	case XNVME_SPEC_FS_OPC_WRITE:
 		res = pwrite(state->fd, dbuf, dbuf_nbytes, ctx->cmd.nvm.slba);
+		sc = res != (ssize_t)dbuf_nbytes ? errno : 0;
 		break;
-
 	case XNVME_SPEC_FS_OPC_READ:
 		res = pread(state->fd, dbuf, dbuf_nbytes, ctx->cmd.nvm.slba);
+		sc = res != (ssize_t)dbuf_nbytes ? errno : 0;
 		break;
 
 	case XNVME_SPEC_NVM_OPC_FLUSH:
 	case XNVME_SPEC_FS_OPC_FLUSH:
 		res = fsync(state->fd);
+		sc = res ? errno : 0;
 		break;
 
 	default:
-		XNVME_DEBUG("FAILED: nosys opcode: %d", ctx->cmd.common.opcode);
-		return -ENOSYS;
+		sc = res = ENOSYS;
+		break;
 	}
 
 	ctx->cpl.result = res;
-	if (res < 0) {
-		XNVME_DEBUG("FAILED: {pread,pwrite,fsync}(), errno: %d", errno);
+	if (sc) {
+		XNVME_DEBUG("FAILED: OPC(%d){pread,pwrite,fsync}(), errno: %d",
+			    ctx->cmd.common.opcode, sc);
 		ctx->cpl.result = 0;
-		ctx->cpl.status.sc = errno;
+		ctx->cpl.status.sc = sc;
 		ctx->cpl.status.sct = XNVME_STATUS_CODE_TYPE_VENDOR;
-		return -errno;
+		return -sc;
+	}
+
+	return 0;
+}
+
+int
+xnvme_be_cbi_sync_psync_cmd_iov(struct xnvme_cmd_ctx *ctx, struct iovec *dvec, size_t dvec_cnt,
+				size_t dvec_nbytes, struct iovec *XNVME_UNUSED(mvec),
+				size_t XNVME_UNUSED(mvec_cnt), size_t XNVME_UNUSED(mvec_nbytes))
+{
+	struct xnvme_be_cbi_state *state = (void *)ctx->dev->be.state;
+	const uint64_t ssw = ctx->dev->geo.ssw;
+	ssize_t res;
+	uint8_t sc;
+
+	switch (ctx->cmd.common.opcode) {
+	case XNVME_SPEC_NVM_OPC_WRITE:
+		res = pwritev(state->fd, dvec, dvec_cnt, ctx->cmd.nvm.slba << ssw);
+		sc = res != (ssize_t)dvec_nbytes ? errno : 0;
+		break;
+
+	case XNVME_SPEC_NVM_OPC_READ:
+		res = preadv(state->fd, dvec, dvec_cnt, ctx->cmd.nvm.slba << ssw);
+		sc = res != (ssize_t)dvec_nbytes ? errno : 0;
+		break;
+
+	case XNVME_SPEC_FS_OPC_WRITE:
+		res = pwritev(state->fd, dvec, dvec_cnt, ctx->cmd.nvm.slba);
+		sc = res != (ssize_t)dvec_nbytes ? errno : 0;
+		break;
+
+	case XNVME_SPEC_FS_OPC_READ:
+		res = preadv(state->fd, dvec, dvec_cnt, ctx->cmd.nvm.slba);
+		sc = res != (ssize_t)dvec_nbytes ? errno : 0;
+		break;
+
+	case XNVME_SPEC_NVM_OPC_FLUSH:
+	case XNVME_SPEC_FS_OPC_FLUSH:
+		res = fsync(state->fd);
+		sc = res ? errno : 0;
+		break;
+
+	default:
+		sc = res = ENOSYS;
+		break;
+	}
+
+	ctx->cpl.result = res;
+	if (sc) {
+		XNVME_DEBUG("FAILED: OPC(%d){pread,pwrite,fsync}(), errno: %d",
+			    ctx->cmd.common.opcode, sc);
+		ctx->cpl.result = 0;
+		ctx->cpl.status.sc = sc;
+		ctx->cpl.status.sct = XNVME_STATUS_CODE_TYPE_VENDOR;
+		return -sc;
 	}
 
 	return 0;
@@ -65,7 +126,7 @@ struct xnvme_be_sync g_xnvme_be_cbi_sync_psync = {
 	.id = "psync",
 #ifdef XNVME_BE_CBI_SYNC_PSYNC_ENABLED
 	.cmd_io = xnvme_be_cbi_sync_psync_cmd_io,
-	.cmd_iov = xnvme_be_nosys_sync_cmd_iov,
+	.cmd_iov = xnvme_be_cbi_sync_psync_cmd_iov,
 #else
 	.cmd_io = xnvme_be_nosys_sync_cmd_io,
 	.cmd_iov = xnvme_be_nosys_sync_cmd_iov,
