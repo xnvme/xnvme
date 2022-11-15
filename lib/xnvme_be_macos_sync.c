25cd19a35a61f09783476ea9e6a44660b0e2a8e7
Simon A. F. Lund <simon.lund@samsung.com>
2022-11-15 18:06:39 +0100

    be/macos/sync: add cmd_iov() via CBI
    Support for iovec payloads was missing in the macOS sync implementation.
    This change adds it. It utilizes the CBI; however, only partially as it
    has to accommodate the macOS-specific behavior of fsync().
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- lib/xnvme_be_macos_sync.c
+++ lib/xnvme_be_macos_sync.c
@@ -1,5 +1,10 @@
 // Copyright (C) Simon A. F. Lund <simon.lund@samsung.com>
 // SPDX-License-Identifier: Apache-2.0
+/**
+ * On MacOS, fsync() does not behave in the same manner as on Linux and FreeBSD, e.g. a fsync()
+ * does not flush to the storage device. To achieve the same behavior fcntl(..., F_FULLFSYNC) is
+ * needed. To normalize the behavior, this is is applied here for cmd_io() and cmd_iov().
+ */
 #include <xnvme_be.h>
 #include <xnvme_be_nosys.h>
 #ifdef XNVME_BE_MACOS_ENABLED
@@ -16,12 +21,9 @@ cmd_io(struct xnvme_cmd_ctx *ctx, void *dbuf, size_t dbuf_nbytes, void *mbuf, si
 	struct xnvme_be_cbi_state *state = (void *)ctx->dev->be.state;
 	ssize_t res;
 
-	///< NOTE: opcode-dispatch (io)
 	switch (ctx->cmd.common.opcode) {
 	case XNVME_SPEC_NVM_OPC_FLUSH:
 	case XNVME_SPEC_FS_OPC_FLUSH:
-		// NOTE: macOS requires special handling for fsync:
-		// https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/fsync.2.html#//apple_ref/doc/man/2/fsync
 		res = fsync(state->fd);
 		ctx->cpl.result = res;
 		if (res < 0) {
@@ -46,13 +48,49 @@ cmd_io(struct xnvme_cmd_ctx *ctx, void *dbuf, size_t dbuf_nbytes, void *mbuf, si
 		return xnvme_be_cbi_sync_psync_cmd_io(ctx, dbuf, dbuf_nbytes, mbuf, mbuf_nbytes);
 	}
 }
+
+int
+cmd_iov(struct xnvme_cmd_ctx *ctx, struct iovec *dvec, size_t dvec_cnt, size_t dvec_nbytes,
+	struct iovec *mvec, size_t mvec_cnt, size_t mvec_nbytes)
+{
+	struct xnvme_be_cbi_state *state = (void *)ctx->dev->be.state;
+	ssize_t res;
+
+	switch (ctx->cmd.common.opcode) {
+	case XNVME_SPEC_NVM_OPC_FLUSH:
+	case XNVME_SPEC_FS_OPC_FLUSH:
+		res = fsync(state->fd);
+		ctx->cpl.result = res;
+		if (res < 0) {
+			XNVME_DEBUG("FAILED: {fsync}(), errno: %d", errno);
+			ctx->cpl.result = 0;
+			ctx->cpl.status.sc = errno;
+			ctx->cpl.status.sct = XNVME_STATUS_CODE_TYPE_VENDOR;
+			return -errno;
+		}
+
+		fcntl(state->fd, F_FULLFSYNC);
+		ctx->cpl.result = res;
+		if (res < 0) {
+			XNVME_DEBUG("FAILED: {F_FULLFSYNC}(), errno: %d", errno);
+			ctx->cpl.result = 0;
+			ctx->cpl.status.sc = errno;
+			ctx->cpl.status.sct = XNVME_STATUS_CODE_TYPE_VENDOR;
+			return -errno;
+		}
+		return 0;
+	default:
+		return xnvme_be_cbi_sync_psync_cmd_iov(ctx, dvec, dvec_cnt, dvec_nbytes, mvec,
+						       mvec_cnt, mvec_nbytes);
+	}
+}
 #endif
 
 struct xnvme_be_sync g_xnvme_be_macos_sync_psync = {
 	.id = "macos",
 #ifdef XNVME_BE_MACOS_ENABLED
 	.cmd_io = cmd_io,
-	.cmd_iov = xnvme_be_nosys_sync_cmd_iov,
+	.cmd_iov = cmd_iov,
 #else
 	.cmd_io = xnvme_be_nosys_sync_cmd_io,
 	.cmd_iov = xnvme_be_nosys_sync_cmd_iov,
