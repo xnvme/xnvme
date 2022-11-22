670d1559d1d5e622bdc1ae0be234aa1c43fdf6e4
Niclas Hedam <niclas@hed.am>
2022-11-22 16:54:56 +0100

    mk,ci,docs,toolbox/pkgs/macos: add macOS 12 and MACOSX_DEPLOYMENT_TARGET
    On macOS compiler-warnings are emitted as deprecated API macros are
    used. One can work around that by setting MACOSX_DEPLOYMENT_TARGET to
    the version that the code utilizes.
    
    This change does so, telling users about MACOSX_DEPLOYMENT_TARGET, in
    the documentation / toolbox and also sets it per default in the
    Makefile-helper.
    
    Once, macOS 11 goes EOL (Nov. 2023), then this could be revisited.
    
    Signed-off-by: Niclas Hedam <niclas@hed.am>

---
+++ toolbox/pkgs/macos-12.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Install packages via brew, assuming that brew is: installed, updated, and upgraded
+clang-format --version && echo "Installed" || brew install clang-format
+git --version && echo "Installed" || brew install git
+python3 --version && echo "Installed" || brew install python3
+meson --version && echo "Installed" || brew install meson
