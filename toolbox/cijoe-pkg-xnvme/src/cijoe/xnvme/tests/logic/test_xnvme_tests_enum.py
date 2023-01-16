f07b88bb62af555256affd54b8c9cab2edf3f927
Karl Bonde Torp <k.torp@samsung.com>
2023-01-16 15:39:05 +0100

    ci,toolbox/cijoe: don't skip test_open and test_multi
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- toolbox/cijoe-pkg-xnvme/src/cijoe/xnvme/tests/logic/test_xnvme_tests_enum.py
+++ toolbox/cijoe-pkg-xnvme/src/cijoe/xnvme/tests/logic/test_xnvme_tests_enum.py
@@ -5,7 +5,9 @@ from ..conftest import XnvmeDriver, xnvme_parametrize
 
 @xnvme_parametrize(labels=["dev"], opts=["be"])
 def test_open(cijoe, device, be_opts, cli_args):
-    pytest.skip(reason="Currently not implemented, comments this out after merge.")
+
+    if be_opts["be"] == "ramdisk":
+        pytest.skip(reason="[be=ramdisk] does not support enumeration")
 
     err, _ = cijoe.run(f"xnvme_tests_enum open --count 4 --be {be_opts['be']}")
     assert not err
@@ -24,7 +26,9 @@ def test_open_all_be(cijoe):
 
 @xnvme_parametrize(labels=["dev"], opts=["be"])
 def test_multi(cijoe, device, be_opts, cli_args):
-    pytest.skip(reason="Currently not implemented, comments this out after merge.")
+
+    if be_opts["be"] == "ramdisk":
+        pytest.skip(reason="[be=ramdisk] does not support enumeration")
 
     err, _ = cijoe.run(f"xnvme_tests_enum multi --count 4 --be {be_opts['be']}")
     assert not err
