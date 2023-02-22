547b1a397e987d70e7e29d719bc7d2edd04ebefd
Karl Bonde Torp <k.torp@samsung.com>
2023-02-22 12:41:30 +0100

    toolbox/pkgs/mac: install pkg-config
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- toolbox/pkgs/macos-11.sh
+++ toolbox/pkgs/macos-11.sh
@@ -5,3 +5,4 @@ clang-format --version && echo "Installed" || brew install clang-format
 git --version && echo "Installed" || brew install git
 python3 --version && echo "Installed" || brew install python3
 meson --version && echo "Installed" || brew install meson
+pkg-config --version && echo "Installed" || brew install pkg-config
