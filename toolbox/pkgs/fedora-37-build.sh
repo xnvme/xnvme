c488f1c3c00b75bfe9752efa91d2a0fbe24d272a
Karl Bonde Torp <k.torp@samsung.com>
2023-03-03 14:22:54 +0100

    build/fedora,centos9: build with '--prefix=/usr'
    This is to ease the dynamic linking
    
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

---
+++ toolbox/pkgs/fedora-37-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# configure xNVMe and build dependencies (fio, libvfn, and SPDK)
+meson setup builddir --prefix=/usr
+
+# build xNVMe
+meson compile -C builddir
+
+# install xNVMe
+meson install -C builddir
+
+# uninstall xNVMe
+# cd builddir && meson --internal uninstall
