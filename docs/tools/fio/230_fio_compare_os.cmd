1af8f9122f110b6e9f4c37540504a14f2b29ca93
Karl Bonde Torp <k.torp@samsung.com>
2023-03-20 12:55:02 +0100

    docgen: remove commands from external fio-engine
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- docs/tools/fio/230_fio_compare_os.cmd
+++
@@ -1,7 +0,0 @@
-fio \
- /usr/local/share/xnvme/xnvme-compare.fio \
- --section=default \
- --ioengine=external:/usr/local/lib/x86_64-linux-gnu/libxnvme-fio-engine.so \
- --filename=/dev/nvme0n1 \
- --xnvme_async=thrpool \
- --xnvme_sync=nvme
