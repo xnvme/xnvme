d1158d45270141f53dd9dc1b75f49879756d3eb1
Simon A. F. Lund <simon.lund@samsung.com>
2023-04-13 23:14:47 +0200

    cijoe: add testing of library linking and loading
    * configs: add 'build.compiler.bin' to configs
      This is needed since Linux uses gcc, FreeBSD clang, and Windows (at
      some point) probably something third
    
    * auxiliary: c sources dynamically loading xNVMe and for linking
    
    * tests utilizing the above, invoking the compiler on the sources,
      running the executables and checking their size
    
    * workflows, the ramdisk workflow is expanded, the other workflows will
      already automatically collect them
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ cijoe/auxiliary/xnvme_linking.c
@@ -0,0 +1,27 @@
+#include <libxnvme.h>
+#include <libxnvme_pp.h>
+
+int
+enum_cb(struct xnvme_dev *dev, void *cb_args)
+{
+	const struct xnvme_ident *ident = xnvme_dev_get_ident(dev);
+
+	xnvme_ident_pr(ident, XNVME_PR_DEF);
+
+	return XNVME_ENUMERATE_DEV_CLOSE;
+}
+
+int
+main(int argc, const char *argv[])
+{
+	struct xnvme_opts opts = xnvme_opts_default();
+	int err;
+
+	err = xnvme_enumerate(argc > 2 ? argv[2] : NULL, &opts, enum_cb, NULL);
+	if (err) {
+		printf("FAILED: enumerate(), err: %d", err);
+		err = EXIT_FAILURE;
+	}
+
+	return 0;
+}
