1e22d3a562e81e05bfdb98b64665715b44795167
Karl Bonde Torp <k.torp@samsung.com>
2023-05-30 16:24:35 +0200

    feat(pkgs/emitter): enable ordering of dependencies
    Now the "alt" supports changing the order of dependencies.  This is to
    support a case where we want to install Python from source, Meson from
    pip, and libvfn from source.
    
    To achieve this `emit.py` and `deps.yaml` have been refactored.
    
    Now, the format for specifying a dependency in `deps.yaml` has been
    changed to a list of lists. Where the inner list contains either "src",
    "sys" or "pip", and the aliases for the deps.
    
    The order of the inner lists is the order in which the deps will be
    installed.
    
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- toolbox/pkgs/emitter/emit.py
+++ toolbox/pkgs/emitter/emit.py
@@ -50,55 +50,68 @@ def load_deps_transformed():
     with (Path(__file__).parent / DEP_PATH).resolve().open("r") as dpath:
         content = yaml.safe_load(dpath)
 
-    origins = {a: dep for c in ["tools", "libs"] for a, dep in content[c].items()}
+    origins = {a: dep for a, dep in content["deps"].items()}
 
     for platform, ver in ((p, v) for p in content["platforms"] for v in p["ver"]):
         apps = content["apps"]  # TODO: filter this based on apps on platform..
 
         # Populate with defaults
-        plain = {}
-        for category in ["tools", "libs"]:
-            for origin, aliases in content["defaults"][platform["os"]][
-                category
-            ].items():
-                if origin not in plain:
-                    plain[origin] = []
-                for alias in aliases:
-                    if alias in platform["drop"]:
-                        continue
-                    plain[origin].append(alias)
-                plain[origin] = list(set(plain[origin]))
+        plain = copy.deepcopy(content["defaults"][platform["os"]]["deps"])
+
+        # Remove some deps depending on plaform
+        for origin, aliases in plain:
+            for alias in platform["drop"]:
+                if alias in aliases:
+                    aliases.remove(alias)
+
+        # Add platform specific auxiliary tools and libs
+        plain += platform["aux"]
 
         # Adjust to "alternative" origins
-        for origin, aliases in platform["alt"].items():
-            plain["sys"] = list(set(plain["sys"]) - set(aliases))
-            plain[origin] = aliases
+        for org_origin, org_aliases in plain:
+            for alt_origin, alt_aliases in platform["alt"]:
+                for alt_alias in alt_aliases:
+                    if alt_alias in org_aliases:
+                        org_aliases.remove(alt_alias)
+
+        plain += platform["alt"]
+
+        # Find the first entry of "sys"
+        first_sys_entry = -1
+        for i, [origin, aliases] in enumerate(plain):
+            if origin == "sys":
+                first_sys_entry = i
+                break
+
+        # Merge all system packages into first entry of "sys"
+        for i in reversed(range(len(plain))):
+            if plain[i][0] == "sys" and i != first_sys_entry:
+                plain[first_sys_entry][1] += plain[i][1]
+                del plain[i]
 
         # Resolve the plain into deps by retrieving the names as per "origin"
-        deps = {"sys": copy.deepcopy(platform["aux"])}
-        for origin, aliases in plain.items():
-            if origin not in deps:
-                deps[origin] = []
-
+        deps = []
+        for origin, aliases in plain:
+            deps_aliases = []
             for alias in aliases:  # Retrieve the package-name from alias
                 pkg_names = origins.get(alias, {}).get("names", {})
                 if origin == "sys":
                     key = platform["mng"] if platform["mng"] in pkg_names else "default"
-                    deps[origin] += pkg_names.get(key, [])
+                    deps_aliases += pkg_names.get(key, [alias])
                 elif origin == "src":
-                    deps[origin].append(alias)
+                    deps_aliases.append(alias)
                     continue
                 else:
-                    deps[origin] += pkg_names.get(origin, [])
+                    deps_aliases += pkg_names.get(origin, [alias])
                     continue
-
-            deps[origin].sort()
+            deps_aliases.sort()
+            deps.append([origin, deps_aliases])
 
         yield platform, ver, apps, deps
 
 
 def emit_scripts(templates):
-    """Fooo"""
+    """Emit shell scripts for each platform"""
 
     for platform, ver, apps, deps in load_deps_transformed():
         shell = "batch" if platform["os"] in ["windows"] else "shell"
@@ -109,35 +122,32 @@ def emit_scripts(templates):
         lines = templates["script"][shell]["default"].render()
         lines += "\n\n"
 
-        if "sys" in deps and deps["sys"]:
-            lines += (
-                templates["pkgman"][platform["mng"]]
-                .get(ident, templates["pkgman"][platform["mng"]]["default"])
-                .render(apps=apps, deps=deps["sys"], platform=platform)
-            )
-            lines += "\n\n"
+        for origin, aliases in deps:
+            if not aliases:
+                continue
 
-        if "src" in deps:
-            for name in deps["src"]:
+            if origin == "sys":
                 lines += (
-                    templates["src"][name]
-                    .get(ident, templates["src"][name]["default"])
-                    .render(apps=apps, deps=deps["sys"], platform=platform)
+                    templates["pkgman"][platform["mng"]]
+                    .get(ident, templates["pkgman"][platform["mng"]]["default"])
+                    .render(apps=apps, deps=aliases, platform=platform)
                 )
                 lines += "\n\n"
-
-        for pkgman, packages in deps.items():
-            if pkgman in ["sys", "src"]:
-                continue
-            if not packages:
-                continue
-
-            lines += (
-                templates["pkgman"][pkgman]
-                .get(ident, templates["pkgman"][pkgman]["default"])
-                .render(apps=apps, deps=deps[pkgman], platform=platform)
-            )
-            lines += "\n\n"
+            elif origin == "src":
+                for alias in aliases:
+                    lines += (
+                        templates["src"][alias]
+                        .get(ident, templates["src"][alias]["default"])
+                        .render(apps=apps, deps=aliases, platform=platform)
+                    )
+                    lines += "\n\n"
+            else:
+                lines += (
+                    templates["pkgman"][origin]
+                    .get(ident, templates["pkgman"][origin]["default"])
+                    .render(apps=apps, deps=aliases, platform=platform)
+                )
+                lines += "\n\n"
 
         with script_filename.resolve().open("w") as script:
             script.write(lines)
