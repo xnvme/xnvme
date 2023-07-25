3c93b9e783dc64dd1c34d30d99ea46e3d6654b13
Karl Bonde Torp <k.torp@samsung.com>
2023-07-25 16:50:37 +0200

    fix(cijoe/kvs): remove unneeded skip
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- cijoe/tests/logic/test_xnvme_tests_kvs.py
+++ cijoe/tests/logic/test_xnvme_tests_kvs.py
@@ -7,17 +7,11 @@ pytest.skip(allow_module_level=True, reason="Not implemented")
 
 @xnvme_parametrize(labels=["kvs"], opts=["be", "admin"])
 def test_kvs_io(cijoe, device, be_opts, cli_args):
-    if be_opts["sync"] in ["block", "psync"]:
-        pytest.skip(reason="Linux Block layer does not support simple-copy")
-
     err, _ = cijoe.run(f"xnvme_tests_kvs kvs_io {cli_args}")
     assert not err
 
 
 @xnvme_parametrize(labels=["kvs"], opts=["be", "admin"])
 def test_kvs_io_custom_kv(cijoe, device, be_opts, cli_args):
-    if be_opts["sync"] in ["block", "psync"]:
-        pytest.skip(reason="Linux Block layer does not support simple-copy")
-
     err, _ = cijoe.run(f"xnvme_tests_kvs kvs_io {cli_args} --key tango --value charlie")
     assert not err
