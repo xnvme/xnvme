f9786ca4385d1cef25c515f96a67dfe996b14ac6
Karl Bonde Torp <k.torp@samsung.com>
2023-07-25 16:49:51 +0200

    feat(cijoe/kvs): test key/value provided as args
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- cijoe/tests/logic/test_xnvme_tests_kvs.py
+++ cijoe/tests/logic/test_xnvme_tests_kvs.py
@@ -12,3 +12,12 @@ def test_kvs_io(cijoe, device, be_opts, cli_args):
 
     err, _ = cijoe.run(f"xnvme_tests_kvs kvs_io {cli_args}")
     assert not err
+
+
+@xnvme_parametrize(labels=["kvs"], opts=["be", "admin"])
+def test_kvs_io_custom_kv(cijoe, device, be_opts, cli_args):
+    if be_opts["sync"] in ["block", "psync"]:
+        pytest.skip(reason="Linux Block layer does not support simple-copy")
+
+    err, _ = cijoe.run(f"xnvme_tests_kvs kvs_io {cli_args} --key tango --value charlie")
+    assert not err
