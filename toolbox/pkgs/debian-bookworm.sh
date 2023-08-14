8854f788e1097d0dd1107a9dffe919d47caa0e32
Karl Bonde Torp <k.torp@samsung.com>
2023-08-14 17:06:08 +0200

    feat(toolbox/pkgs): build SPDK from src
    This includes installing both SPDK and DPDK from src.
    This is to make sure that xNVMe is able to find the DPDK libraries on
    runtime. If DPDK is only built through SPDK, the libraries are never
    installed and thus cannot be located through LD.
    
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- toolbox/pkgs/debian-bookworm.sh
+++ toolbox/pkgs/debian-bookworm.sh
@@ -42,6 +42,34 @@ apt-get -qy install \
  uuid-dev
 
 #
+# Clone, build and install dpdk
+#
+# Assumptions:
+#
+# - These commands are executed with sufficient privileges (sudo/root)
+#
+git clone https://github.com/dpdk/dpdk.git toolbox/third-party/dpdk/repository
+pushd toolbox/third-party/dpdk/repository 
+
+git checkout v22.11
+
+# 'cpu_instruction_set=generic' is used for cross compilation reasons for optimal performance use 'cpu_instruction_set=native'
+meson setup builddir --prefix=/usr \
+-Dc_args="-fPIC -Wno-error  -Wno-stringop-overflow -fcommon -Wno-stringop-overread -Wno-array-bounds" \
+-Dcpu_instruction_set=generic \
+-Ddisable_libs=gro,node,cfgfile,pdump,bitratestats,flow_classify,table,port,latencystats,kni,metrics,pipeline,jobstats,gpudev,gso,regexdev \
+-Ddisable_apps=pdump,test-cmdline,test,test-regex,test-gpudev,test-fib,test-pipeline,test-sad,test-acl,test-bbdev,proc-info,test-eventdev,test-flow-perf,test-pmd,dumpcap,test-compress-perf,test-crypto-perf,test-security-perf \
+-Denable_docs=false \
+-Denable_drivers=bus,bus/pci,bus/vdev,mempool/ring \
+-Denable_kmods=false \
+-Dtests=false
+
+
+meson compile -C builddir
+meson install -C builddir
+popd
+
+#
 # Clone, build and install libvfn
 #
 # Assumptions:
@@ -56,5 +84,44 @@ git checkout v2.0.2
 meson setup builddir -Dlibnvme="disabled" -Ddocs="disabled" --prefix=/usr
 meson compile -C builddir
 meson install -C builddir
+popd
+
+#
+# Clone, build and install spdk
+#
+# Assumptions:
+#
+# - These commands are executed with sufficient privileges (sudo/root)
+#
+git clone https://github.com/spdk/spdk.git toolbox/third-party/spdk/repository
+
+pushd toolbox/third-party/spdk/repository 
+git checkout v23.05
+git submodule update --init --recursive
+
+./configure \
+  --disable-tests \
+  --disable-unit-tests \
+  --disable-examples \
+  --disable-apps \
+  --without-crypto \
+  --without-fuse \
+  --without-idxd \
+  --without-iscsi-initiator \
+  --without-nvme-cuse \
+  --without-ocf \
+  --without-rbd \
+  --without-uring \
+  --without-usdt \
+  --without-vhost \
+  --without-vtune \
+  --without-virtio \
+  --without-xnvme \
+  --with-shared \
+  --with-dpdk=/usr \
+  --prefix=/usr
+
+make
+make install
 popd
 
--- toolbox/pkgs/freebsd-13.sh
+++ toolbox/pkgs/freebsd-13.sh
@@ -34,3 +34,75 @@ python3 -m ensurepip --upgrade
 # a "File name too long", 0.60 seems ok.
 python3 -m pip install meson==0.60
 
+#
+# Clone, build and install dpdk
+#
+# Assumptions:
+#
+# - These commands are executed with sufficient privileges (sudo/root)
+#
+git clone https://github.com/dpdk/dpdk.git toolbox/third-party/dpdk/repository
+pushd toolbox/third-party/dpdk/repository 
+
+git checkout v22.11
+
+meson setup builddir --prefix=/usr \
+-Dc_args="-fPIC -Wno-error" \
+-Dcpu_instruction_set=native \
+-Ddisable_libs=bitratestats,cfgfile,flow_classify,gpudev,gro,gso,jobstats,kni,latencystats,metrics,node,pdump,pipeline,port,table \
+-Ddisable_apps=pdump,test-cmdline,test,test-regex,test-gpudev,test-fib,test-pipeline,test-sad,test-acl,test-bbdev,proc-info,test-eventdev,test-flow-perf,test-pmd,dumpcap,test-compress-perf,test-crypto-perf,test-security-perf \
+-Denable_docs=false \
+-Denable_drivers=bus,bus/pci,bus/vdev,mempool/ring \
+-Denable_kmods=true \
+-Dtests=false
+
+
+meson compile -C builddir
+meson install -C builddir
+popd
+
+#
+# Clone, build and install spdk
+#
+# Assumptions:
+#
+# - These commands are executed with sufficient privileges (sudo/root)
+#
+git clone https://github.com/spdk/spdk.git toolbox/third-party/spdk/repository
+
+pushd toolbox/third-party/spdk/repository 
+git checkout v23.05
+git submodule update --init --recursive
+
+./configure \
+  --disable-tests \
+  --disable-unit-tests \
+  --disable-examples \
+  --disable-apps \
+  --without-crypto \
+  --without-fuse \
+  --without-idxd \
+  --without-iscsi-initiator \
+  --without-nvme-cuse \
+  --without-ocf \
+  --without-rbd \
+  --without-uring \
+  --without-usdt \
+  --without-vhost \
+  --without-vtune \
+  --without-virtio \
+  --without-xnvme \
+  --with-shared \
+  --with-dpdk=/usr \
+  --prefix=/usr
+
+gmake
+gmake install
+
+# SPDK doesn't install to the correct pkg-config directory - thus they need to be moved manually
+# Alternatively use the command 'export PKG_CONFIG_PATH=/usr/libdata/pkgconfig'
+
+cp /usr/lib/pkgconfig/spdk* /usr/libdata/pkgconfig/
+
+popd
+
