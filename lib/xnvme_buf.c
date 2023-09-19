b70a60530196fdb349318df5f4f8fdeedd7d6f9d
Simon A. F. Lund <simon.lund@samsung.com>
2023-09-19 18:31:52 +0200

    fix: use zu format-specifier for size_t
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- lib/xnvme_buf.c
+++ lib/xnvme_buf.c
@@ -252,12 +252,11 @@ xnvme_buf_diff_pr(const void *expected, const void *actual, size_t nbytes, int X
 		}
 
 		++diff;
-		printf("    - {byte: '%06" PRIu64 "', expected: 0x%" PRIx8 ", actual: 0x%" PRIx8
-		       ")\n",
-		       i, exp[i], act[i]);
+		printf("    - {byte: '%06zu', expected: 0x%" PRIx8 ", actual: 0x%" PRIx8 ")\n", i,
+		       exp[i], act[i]);
 	}
-	printf("  nbytes: %" PRIu64 "\n", nbytes);
-	printf("  nbytes_diff: %" PRIu64 "\n", diff);
+	printf("  nbytes: %zu\n", nbytes);
+	printf("  nbytes_diff: %zu\n", diff);
 }
 
 int
