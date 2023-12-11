f655ffaafac328bff8e049106927723f6ccf8fc3
Karl Bonde Torp <k.torp@samsung.com>
2023-12-11 15:35:36 +0100

    feat(cijoe): add testing of LBA Format
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- cijoe/tests/logic/test_xnvme_tests_lblk.py
+++ cijoe/tests/logic/test_xnvme_tests_lblk.py
@@ -25,3 +25,19 @@ def test_write_zeroes(cijoe, device, be_opts, cli_args):
 
     err, _ = cijoe.run(f"xnvme_tests_lblk write_zeroes {cli_args}")
     assert not err
+
+
+@xnvme_parametrize(labels=["dev"], opts=["be", "admin", "sync"])
+def test_lba_format(cijoe, device, be_opts, cli_args):
+    if be_opts["be"] == "linux":
+        pytest.skip(
+            reason="Linux Kernel cannot handle formatting namespaces in quick succession"
+        )
+    if be_opts["be"] == "fbsd" and be_opts["sync"] in ["nvme"]:
+        pytest.skip(
+            reason=f"Not supported: separate mbuf via { be_opts['sync']} on {be_opts['be']}"
+        )
+    if be_opts["be"] == "ramdisk":
+        pytest.skip(reason=f"Not supported: xnvme_adm_format via { be_opts['be']}")
+    err, _ = cijoe.run(f"xnvme_tests_lblk lba_format {cli_args}")
+    assert not err
