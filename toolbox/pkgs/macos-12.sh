4aeed487121e38541ec2f737932b32578ed80ee7
Simon A. F. Lund <simon.lund@samsung.com>
2023-12-14 21:26:02 +0100

    feat(toolbox/pkgs): brew-install without updating and ignore failure
    On Linux, we guard against sporadic packaging failures by using Docker
    images where dependencies are installed; thus, in case things are
    failing, then docker images are just not updated, and CI won't be
    blocked.
    
    With macOS, we are less fortunate; thus, to avoid brew/packages blocking
    the xNVMe pipelines, this change prints out an error message instead
    of halting.
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- toolbox/pkgs/macos-12.sh
+++ toolbox/pkgs/macos-12.sh
@@ -7,17 +7,18 @@ ldd --version || true
 
 # This is done to avoid appleframework deprecation warnings
 export MACOSX_DEPLOYMENT_TARGET=11.0
+export HOMEBREW_NO_AUTO_UPDATE=1
 
 # Install packages via brew, assuming that brew is: installed, updated, and upgraded
-clang-format --version && echo "Installed" || brew install clang-format
-git --version && echo "Installed" || brew install git
+clang-format --version && echo "Installed" || brew install clang-format --overwrite || echo "Failed installing"
+git --version && echo "Installed" || brew install git --overwrite || echo "Failed installing"
 if make --version | grep i386-apple; then
   brew install make
 fi
 
-meson --version && echo "Installed" || brew install meson
-pkg-config --version && echo "Installed" || brew install pkg-config
-python3 --version && echo "Installed" || brew install python3
+meson --version && echo "Installed" || brew install meson --overwrite || echo "Failed installing"
+pkg-config --version && echo "Installed" || brew install pkg-config --overwrite || echo "Failed installing"
+python3 --version && echo "Installed" || brew install python3 --overwrite || echo "Failed installing"
 
 # Install packages via the Python package-manager (pip)
 python3 -m pip install --upgrade pip
