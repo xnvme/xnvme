a616465b3454c04baf1031b6ac850ab4ef097794
Simon A. F. Lund <simon.lund@samsung.com>
2024-01-23 15:59:39 +0100

    fix(docs/tutorial/fdp): add fio example 'xnvme-fdp.fio'
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- docs/tutorial/fdp/001_xnvme_fdp_fio.cmd
+++ docs/tutorial/fdp/001_xnvme_fdp_fio.cmd
@@ -1,1 +1,1 @@
-fio examples/xnvme-fdp.fio --section=default --ioengine=xnvme --xnvme_async=io_uring_cmd --filename=/dev/ng3n1
+fio ../tutorial/fdp/examples/xnvme-fdp.fio --section=default --ioengine=xnvme --xnvme_async=io_uring_cmd --filename=/dev/ng3n1
