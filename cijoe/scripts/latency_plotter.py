9f5eccbd73d80dfafb3eb2b68343002cd0e7619d
Morten B. Rasmussen <m.rasmussen@samsung.com>
2024-01-25 10:32:56 +0100

    feat(benchmark): Add latency benchmark & reporting

---
+++ cijoe/scripts/latency_plotter.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python
+"""
+Extract and transform fio latency measurements
+=============================================
+
+fio has:
+
+* bandwidth in KiB / second
+* latency is mean and in nano-seconds
+* IOPS are 'raw' / base-unit
+
+.. note::
+    The metric-context need addition of backend-options, the options are
+    semi-encoded by ioengine and 'label', however, that is not very precise.
+
+.. note::
+    This uses matplotlib and numpy for plotting
+"""
+import errno
+import hashlib
+import json
+import logging as log
+import os
+import traceback
+from pathlib import Path
+from typing import Dict, List
+
+import matplotlib.pyplot as plt
+from cijoe.core.analyser import to_base_unit
+from cijoe.core.resources import dict_from_yamlfile
+from cijoe.fio.wrapper import dict_from_fio_output_file
+
+JSON_DUMP = {"indent": 4}
+
+FIO_COMPOUND_FILENAME = "fio-output-compound.json"
+FIO_OUTPUT_NORMALIZED_FILENAME = "fio-output-normalize.json"
+
+Data = Dict[str, Dict[str, List[float | int] | str]]
+
+
+def extract_fio(args, cijoe, step):
+    """
+    Traverse 'args.output' for 'fio-output_*.txt' files, and create:
+
+    * A 'fio-output_*.json' per 'fio-output_*.txt'
+    * A single "compound" 'fio-output-compound.json' containing all the discovered
+      JSON-documents with the file.stem as key.
+    """
+
+    search = step.get("with", {}).get("path", args.output)
+    if not search:
+        return errno.EINVAL
+
+    compound = {}
+
+    search = Path(search)
+    for path in search.rglob("fio-output_*.txt"):
+        jsondict = dict_from_fio_output_file(path)
+        with path.with_suffix(".json").open("w") as jfd:
+            json.dump(jsondict, jfd, **JSON_DUMP)
+
+        compound[path.stem] = jsondict
+    artifacts = search / "artifacts"
+
+    with (artifacts / FIO_COMPOUND_FILENAME).open("w") as jfd:
+        json.dump(compound, jfd, **JSON_DUMP)
+        log.info(f"wrote: {jfd.name}")
+
+    return 0
+
+
+def normalize(args, cijoe, step):
+    """Transform the "compound" fio output into series of 'normalized' data"""
+
+    def contextualize(fio_output: dict, stem) -> dict:
+        """Helper function for the transformation of output"""
+
+        assert len(fio_output["jobs"]) == 1
+
+        job = fio_output["jobs"][0]
+        options = job["job options"]
+
+        context = {
+            "name": options["name"],
+            "timestamp": fio_output["timestamp"],
+            "ioengine": options["ioengine"],
+            "bs": options["bs"],
+            "rw": options["rw"],
+            "size": options["size"],
+            "iodepth": int(options["iodepth"]),
+            "filename": options["filename"],
+            "stem": stem,
+        }
+
+        context.update(
+            {
+                key: options[key]
+                for key in [
+                    "spdk_conf",
+                    "xnvme_be",
+                    "xnvme_async",
+                    "xnvme_sync",
+                    "xnvme_admin",
+                    "xnvme_dev",
+                ]
+                if key in options
+            }
+        )
+
+        metrics = {
+            "ctx": context,
+            "iops": to_base_unit(job["read"]["iops_mean"], ""),
+            "bwps": to_base_unit(job["read"]["bw_mean"], "KiB"),
+            "lat": job["read"]["lat_ns"]["mean"],
+            "stddev": to_base_unit(job["read"]["lat_ns"]["stddev"], ""),
+        }
+
+        md5_basis = [
+            f"{key}={val}"
+            for key, val in sorted(context.items())
+            if key not in {"timestamp", "stem", "iodepth", "filename"}
+        ]
+        md5_key = "".join(md5_basis)
+        m = hashlib.md5()
+        m.update(str.encode(md5_key))
+
+        return str(m.hexdigest()), metrics
+
+    search = step.get("with", {}).get("path", args.output)
+    if not search:
+        return errno.EINVAL
+
+    paths = list(Path(search).rglob(FIO_COMPOUND_FILENAME))
+    if len(paths) != 1:
+        log.error(f"Unexpected paths: {paths}")
+        return errno.EINVAL
+
+    compound_path = paths[0]
+    with compound_path.open() as jfd:
+        compound = json.load(jfd)
+
+    collection = [contextualize(results, stem) for stem, results in compound.items()]
+
+    with compound_path.with_name(FIO_OUTPUT_NORMALIZED_FILENAME).open("w") as jfd:
+        json.dump(collection, jfd, **JSON_DUMP)
+
+    return 0
+
+
+def data_as_a_function_of(data, x="iodepth", y="iops") -> Data:
+    """Organize and label data with 'y' points as a function of 'x' points"""
+
+    samples = {}
+    for ident, metrics in data:
+        context = metrics["ctx"]
+
+        if ident not in samples:
+            label = context["name"]
+            samples[ident] = {"xs": [], "ys": [], "ident": ident, "label": label}
+
+        samples[ident]["xs"].append(context[x])
+        samples[ident]["ys"].append(metrics[y])
+
+    return samples
+
+
+def data_as_fixed(data, x="iodepth", y="iops", xval=None):
+    """Organize and label data with 'y' points as a function of 'x' points"""
+
+    samples = {}
+    for ident, metrics in data:
+        context = metrics["ctx"]
+        if str(context[x]) != str(xval):
+            continue
+
+        if ident not in samples:
+            label = context["name"]
+            samples[ident] = {"xs": [], "ys": [], "ident": ident, "label": label}
+
+        samples[ident]["xs"].append(context[x])
+        samples[ident]["ys"].append(metrics[y])
+
+    return samples
+
+
+def plot_attributes_from_step(step):
+    """Load plot-attributes using paths in step"""
+
+    limits_path = Path(step.get("with", {}).get("limits", "plot-limits.yaml"))
+    legends_path = Path(step.get("with", {}).get("legends", "plot-legends.yaml"))
+
+    return {
+        "limits": dict_from_yamlfile(limits_path),
+        "legends": dict_from_yamlfile(legends_path),
+    }
+
+
+def build_plot_groups(data: Data, groups: List[str]):
+    """Group the `data` in context of the `groups` list
+
+    TODO: This functions logic is not sound, it depends on the ordering of the `groups`
+    list. The issue presents itself when a partial match on a label occurs.
+    """
+    grouped = {x: {} for x in groups}
+
+    for ident, samples in data.items():
+        for group in groups:
+            if f"{group}" not in samples["label"]:
+                continue
+            grouped[group][ident] = samples
+            break
+    return grouped
+
+
+def line_plot(args, cijoe, step):
+    search = step.get("with", {}).get("path", args.output)
+    if not search:
+        return errno.EINVAL
+
+    search_for = FIO_OUTPUT_NORMALIZED_FILENAME
+
+    path = next(Path(search).rglob(search_for))
+
+    with path.open() as jfd:
+        data = json.load(jfd)
+
+    plot_attributes = plot_attributes_from_step(step)
+
+    groups = ["io_uring_cmd", "io_uring", "posix", "libaio", "spdk", "null"]
+    x = "iodepth"
+    y = "lat"
+
+    data = data_as_a_function_of(data, x, y)
+
+    grouped = build_plot_groups(data, groups)
+
+    for group, dset in grouped.items():
+        plt.clf()
+
+        for _, samples in dset.items():
+            label = samples["label"]
+            attrs = plot_attributes["legends"].get(label, None)
+            if attrs is None:
+                log.error(f"Missing plot-attributes for label({label})")
+                continue
+
+            xs, ys = [list(t) for t in zip(*sorted(zip(samples["xs"], samples["ys"])))]
+            plt.plot(
+                xs,
+                ys,
+                label=attrs["legend"],
+                color=attrs["color"],
+                marker=attrs["marker"],
+            )
+
+        plt.xlabel(x)
+        plt.ylabel(y)
+
+        plt.legend(
+            bbox_to_anchor=(0, 0.95, 1, 0.2),
+            loc="lower center",
+            ncol=2,
+            facecolor="white",
+            framealpha=1,
+        )
+        plt.ylim(plot_attributes["limits"]["lineplot"]["y_lim"])
+
+        os.makedirs(args.output / "artifacts", exist_ok=True)
+        plt.savefig(args.output / "artifacts" / f"fio_lineplot_{group}.png")
+
+    return 0
+
+
+def main(args, cijoe, step):
+    try:
+        search = step.get("with", {}).get("path", args.output)
+
+        if not search:
+            return errno.EINVAL
+
+        funcs = [extract_fio, normalize, line_plot]
+
+        for func in funcs:
+            err = func(args, cijoe, step)
+            if err:
+                return err
+    except Exception as exc:
+        log.error(f"Something failed({exc})")
+        log.error("".join(traceback.format_exception(None, exc, exc.__traceback__)))
+        print(
+            type(exc).__name__,  # TypeError
+            __file__,  # /tmp/example.py
+            exc.__traceback__.tb_lineno,  # 2
+        )
+        return 1
+
+    return 0
---
+++ cijoe/templates/perf_report/latency.jinja2.rst
@@ -0,0 +1,212 @@
+.. footer::
+
+   ###Page### / ###Total###
+
+.. header::
+
+   ###Title### - ###Section###
+
+====================================
+ xNVMe in SPDK - Latency Report
+====================================
+
+.. contents:: Table of Contents
+   :depth: 2
+
+.. raw:: pdf
+
+   PageBreak oneColumn
+
+Audience
+========
+
+This report is of interest for those curious about:
+
+* Storage Abstraction Layer overhead and encapsulation
+
+  - The efficient Linux interfaces (``io_uring`` and ``io_uring_cmd``)
+  - The legacy Linux interfaces (``libaio``)
+  - And how these can be encapsulated with minimal overhead with **xNVMe**
+
+* The xNVMe project
+
+  - The integration of xNVMe in SPDK via ``bdev_xnvme``
+  - Efficiency of ``bdev_xnvme`` compared to ``bdev_uring`` / ``bdev_aio`` / ``bdev_nvme``
+
+For the report a bit of background on **xNVMe** is needed, as such, this will be provided in the following section.
+
+What is xNVMe?
+--------------
+
+**xNVMe** is a low-level storage abstraction layer. It is designed for
+convenient use of NVMe devices, however, not limited to these. At its core
+**xNVMe** provides an API operating on **commands**, that is, their
+construction, subsmission, and completion. This can be in a synchronous / blocking
+manner, or asynchronously / non-blocking via **queues**.
+
+This **foundational** abstraction based on **commands**, encapsulates the
+command-transport and semantics usually tied to storage interfaces.
+
+That is, with **xNVMe** there is no assumption that the commands are
+read/write commands.
+
+By decomposing the storage interface in this manner, then any API such as the
+blocking ``preadv/pwritev`` system calls, the asynchrous
+``io_uring``/``io_uring_cmd`` system interface, or the rich **NVMe** commands
+and IO queue pairs, can be represented.
+
+And so they are. The API provided by **xNVMe** has implementations on top of:
+``libaio``, ``POSIX aio``, ``io_uring``, ``io_uring_cmd``, **SPDK
+NVMe driver**, **libvfn NVMe driver**, OS ioctl() interfaces, thread pools, and
+async. emulation.
+
+As such, **xNVMe** provides the encapsulation of the storage interfaces of
+operating system provided abstractions, user space NVMe drivers, storage system
+interfaces, and storage system APIs.
+
+Purpose
+-------
+
+With a storage abstraction layer such as **xNVMe** a natural question is: 
+
+..
+
+  What is the cost in **performance** for a system when using **xNVMe**
+  as the storage abstraction layer? 
+
+The purpose of this report is to answer the above question. To do so, we first
+qualify that by performance we will refer to the metric of IO Operations per
+Second (IOPS) on a single saturated CPU.
+
+Thus, in case **xNVMe** has negative impact on IOPS, then we wish to quantity
+how many. And in general, then we wish to compare the effect on achieved IOPS
+rate relatively.
+
+That is, comparing the IOPS with and without using **xNVMe** as the
+storage abstraction layer for the SPDK bdev abstraction.
+
+
+.. raw:: pdf
+
+   PageBreak
+
+
+Test Setup
+==========
+
+.. note:: **TODO**: Write some prose here.
+
+.. raw:: pdf
+
+   PageBreak
+
+Software
+--------
+
+.. note::
+   **TODO**: Populate this section using the artifacts produces and data collected by
+   **CIJOE**, that is, the output from ``cat /boot/cmdline``, ``uname``,
+   ``dmesg``, ``/etc/os-release``.
+
+.. raw:: pdf
+
+   PageBreak
+
+Hardware
+--------
+
+.. note::
+   **TODO**: Populate this section using the artifacts produces by CIJOE, that is, the ``lshw``.
+
+BIOS
+----
+
+.. note::
+   **TODO**: Add BIOS name and version and describe settings changed from the default.
+
+Methodology
+===========
+
+
+
+.. raw:: pdf
+
+   PageBreak
+
+Results
+=======
+
+.. raw:: pdf
+
+   PageBreak
+
+libaio
+------
+
+
+.. image:: fio_lineplot_libaio.png
+   :align: center
+   :width: 100%
+
+
+.. raw:: pdf
+
+   PageBreak
+
+io_uring
+--------
+
+.. image:: fio_lineplot_io_uring.png
+   :align: center
+   :width: 100%
+
+
+.. raw:: pdf
+
+   PageBreak
+
+io_uring_cmd
+------------
+
+
+.. image:: fio_lineplot_io_uring_cmd.png
+   :align: center
+   :width: 100%
+
+.. raw:: pdf
+
+   PageBreak
+
+posixaio
+------
+
+
+.. image:: fio_lineplot_posix.png
+   :align: center
+   :width: 100%
+
+
+.. raw:: pdf
+
+   PageBreak
+
+null
+------
+
+
+.. image:: fio_lineplot_null.png
+   :align: center
+   :width: 100%
+
+
+.. raw:: pdf
+
+   PageBreak
+
+
+spdk
+----
+
+.. image:: fio_lineplot_spdk.png
+   :align: center
+   :width: 100%
