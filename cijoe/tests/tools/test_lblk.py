9380eb61ba649f092d2d4d413f7ae0a0a2f8d262
Karl Bonde Torp <k.torp@samsung.com>
2024-02-01 10:58:39 +0100

    feat(cijoe): run tests for PI devices
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- cijoe/tests/tools/test_lblk.py
+++ cijoe/tests/tools/test_lblk.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ..conftest import XnvmeDriver, xnvme_parametrize
 
 
@@ -47,8 +49,78 @@ def test_write_zeroes(cijoe, device, be_opts, cli_args):
     assert not err
 
 
-@xnvme_parametrize(labels=["write_read_pi"], opts=["be", "admin"])
-def test_write_read_pi(cijoe, device, be_opts, cli_args):
+@xnvme_parametrize(labels=["pi1"], opts=["be", "admin"])
+def test_pi_type1(cijoe, device, be_opts, cli_args):
+    if be_opts["be"] == "linux" and be_opts["admin"] in ["block"]:
+        pytest.skip(reason="[admin=block] does not support metadata")
+
+    err, _ = cijoe.run(
+        f"lblk write-read-pi {cli_args} --slba 0x0 --nlb 0 --pract 1 --prchk 0x5"
+    )
+    assert not err
+
+    err, _ = cijoe.run(
+        f"lblk write-read-pi {cli_args} --slba 0x0 --nlb 4 --prchk 0x7 --apptag 0x1234 --apptag_mask 0xFFFF"
+    )
+    assert not err
+
+
+@xnvme_parametrize(labels=["pi2"], opts=["be", "admin"])
+def test_pi_type2(cijoe, device, be_opts, cli_args):
+    if be_opts["be"] == "linux" and be_opts["admin"] in ["block"]:
+        pytest.skip(reason="[admin=block] does not support metadata")
+
+    err, _ = cijoe.run(
+        f"lblk write-read-pi {cli_args} --slba 0x0 --nlb 0 --pract 1 --prchk 0x5"
+    )
+    assert not err
+
+    err, _ = cijoe.run(
+        f"lblk write-read-pi {cli_args} --slba 0x0 --nlb 4 --prchk 0x7 --apptag 0x1234 --apptag_mask 0xFFFF"
+    )
+    assert not err
+
+
+@xnvme_parametrize(labels=["pi3"], opts=["be", "admin"])
+def test_pi_type3(cijoe, device, be_opts, cli_args):
+    if be_opts["be"] == "linux" and be_opts["admin"] in ["block"]:
+        pytest.skip(reason="[admin=block] does not support metadata")
+
+    # For PI type 3 prchk 0bxx1 is invalid
+    # Thus using prchk 0b100
+    err, _ = cijoe.run(
+        f"lblk write-read-pi {cli_args} --slba 0x0 --nlb 0 --pract 1 --prchk 0x4"
+    )
+    assert not err
+
+    # And prchk 0b110
+    err, _ = cijoe.run(
+        f"lblk write-read-pi {cli_args} --slba 0x0 --nlb 4 --prchk 0x6 --apptag 0x1234 --apptag_mask 0xFFFF"
+    )
+    assert not err
+
+
+@xnvme_parametrize(labels=["pi1_ex"], opts=["be", "admin"])
+def test_pi_type1_extended_lba(cijoe, device, be_opts, cli_args):
+    if be_opts["be"] == "linux" and be_opts["admin"] in ["block"]:
+        pytest.skip(reason="[admin=block] does not support metadata")
+
+    err, _ = cijoe.run(
+        f"lblk write-read-pi {cli_args} --slba 0x0 --nlb 0 --pract 1 --prchk 0x5"
+    )
+    assert not err
+
+    err, _ = cijoe.run(
+        f"lblk write-read-pi {cli_args} --slba 0x0 --nlb 4 --prchk 0x7 --apptag 0x1234 --apptag_mask 0xFFFF"
+    )
+    assert not err
+
+
+@xnvme_parametrize(labels=["pi1_pif2"], opts=["be", "admin"])
+def test_pi1_pif2(cijoe, device, be_opts, cli_args):
+    if be_opts["be"] == "linux" and be_opts["admin"] in ["block"]:
+        pytest.skip(reason="[admin=block] does not support metadata")
+
     err, _ = cijoe.run(
         f"lblk write-read-pi {cli_args} --slba 0x0 --nlb 0 --pract 1 --prchk 0x5"
     )
