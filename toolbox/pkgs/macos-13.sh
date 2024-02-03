a968a6053ea88633fe8af975c32cac374481333c
Simon A. F. Lund <simon.lund@samsung.com>
2024-02-03 20:43:14 +0100

    feat(toolbox/pkgs,cijoe/tests): bump libvfn to v3.0.1
    When bumping libvfn, then the currently skipped test for PI using libvfn
    is re-enabled.can.
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- toolbox/pkgs/macos-13.sh
+++ toolbox/pkgs/macos-13.sh
@@ -12,11 +12,11 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 # Install packages via brew, assuming that brew is: installed, updated, and upgraded
 clang-format --version && echo "Installed" || brew install clang-format --overwrite || echo "Failed installing"
 git --version && echo "Installed" || brew install git --overwrite || echo "Failed installing"
+isa-l --version && echo "Installed" || brew install isa-l --overwrite || echo "Failed installing"
 if make --version | grep i386-apple; then
   brew install make
 fi
 
-isa-l --version && echo "Installed" || brew install isa-l --overwrite || echo "Failed installing"
 meson --version && echo "Installed" || brew install meson --overwrite || echo "Failed installing"
 pkg-config --version && echo "Installed" || brew install pkg-config --overwrite || echo "Failed installing"
 python3 --version && echo "Installed" || brew install python3 --overwrite || echo "Failed installing"
