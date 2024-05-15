cdfc88cdfb37612f3abdc2bb6277975c948fcb19
Simon A. F. Lund <simon.lund@samsung.com>
2024-05-15 10:33:06 +0200

    feat(docs): switch theme to pydatadocs
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- docs/autogen/conf.py
+++ docs/autogen/conf.py
@@ -5,14 +5,11 @@ import os
 
 from xnvme_ver import xnvme_ver
 
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-
 extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.imgmath",
     #    'sphinxcontrib.bibtex',
     "sphinxcontrib.jquery",
-    "sphinx_rtd_theme",
     "breathe",
 ]
 
@@ -27,10 +24,6 @@ release = xnvme_ver(os.path.join("..", "..", "meson.build"))
 
 pygments_style = "sphinx"
 
-if not on_rtd:
-    html_theme = "sphinx_rtd_theme"
-
-html_theme_options = {"analytics_id": "UA-159785887-1"}
 html_css_files = ["theme_overrides.css"]
 html_static_path = [os.path.join("..", "_static")]
 
@@ -40,3 +33,37 @@ htmlhelp_basename = "xnvmedoc"
 breathe_projects = {project: os.path.join("builddir", "doxy", "xml")}
 breathe_default_project = project
 breathe_domain_by_extension = {"h": "c"}
+
+html_logo = "../_static/xnvme.svg"
+
+html_theme_options = {"analytics_id": "UA-159785887-1"}
+
+html_theme = "pydata_sphinx_theme"
+
+html_theme_options = {
+    "navigation_depth": 4,
+    "navbar_align": "left",
+    "header_links_before_dropdown": 7,
+    "show_prev_next": False,
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/xnvme/xnvme",
+            "icon": "fa-brands fa-square-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Discord",
+            "url": "https://discord.gg/XCbBX9DmKf",
+            "icon": "fa-brands fa-discord",
+        },
+    ],
+}
+
+}
+
+html_context = {
+    "default_mode": "light",
+}
+
+html_show_sourcelink = False
