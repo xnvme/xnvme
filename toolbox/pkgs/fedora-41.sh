c900cb2a3d128a4de174f3d90d079fda405e95c7
Karl Bonde Torp <k.torp@samsung.com>
2024-05-28 11:04:37 +0200

    fix(ci/fedora): drop EOL versions
    All of our fedora versions are in EOL, swap them for currently supported
    versions.
    
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

---
+++ toolbox/pkgs/fedora-41.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# Query the linker version
+ld -v || true
+
+# Query the (g)libc version
+ldd --version || true
+
+# Install packages via the system package-manager (dnf)
+dnf install -y \
+ CUnit-devel \
+ autoconf \
+ bash \
+ clang-tools-extra \
+ diffutils \
+ findutils \
+ g++ \
+ gcc \
+ git \
+ isa-l \
+ libaio-devel \
+ libarchive-devel \
+ libtool \
+ liburing \
+ liburing-devel \
+ libuuid-devel \
+ make \
+ meson \
+ nasm \
+ ncurses \
+ numactl-devel \
+ openssl-devel \
+ patch \
+ pipx \
+ pkgconfig \
+ procps \
+ python3-devel \
+ python3-pip \
+ python3-pyelftools \
+ zlib-devel
+
+#
+# Clone, build and install libvfn
+#
+# Assumptions:
+#
+# - Dependencies for building libvfn are met (system packages etc.)
+# - Commands are executed with sufficient privileges (sudo/root)
+#
+git clone https://github.com/SamsungDS/libvfn.git toolbox/third-party/libvfn/repository
+
+pushd toolbox/third-party/libvfn/repository
+git checkout v4.0.1
+meson setup builddir -Dlibnvme="disabled" -Ddocs="disabled" --buildtype=release --prefix=/usr
+meson compile -C builddir
+meson install -C builddir
+popd
+
