1160c5aa3a44d8df3fab6bca76a3aed6917a0079
Karl Bonde Torp <k.torp@samsung.com>
2024-06-07 14:25:36 +0200

    fix(cijoe/tests/enum): leverage cijoe infra
    Instead of having tests that manually toggle the driver, leverage the
    builtin functionality of doing so.
    This makes skipping tests on specific backends easier, as well as making
    grouping the tests clearer, i.e. not running a userspace test in the
    middle of kernel tests.
    
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- cijoe/tests/logic/test_xnvme_tests_enum.py
+++ cijoe/tests/logic/test_xnvme_tests_enum.py
@@ -17,18 +17,10 @@ def test_open(cijoe, device, be_opts, cli_args):
     assert not err
 
 
-def test_open_all_be(cijoe):
-    XnvmeDriver.kernel_attach(cijoe)
-    err, _ = cijoe.run("xnvme_tests_enum open --count 4")
-    assert not err
-
-    XnvmeDriver.kernel_detach(cijoe)
-    err, _ = cijoe.run("xnvme_tests_enum open --count 4")
-    assert not err
-
-
 @xnvme_parametrize(labels=["dev"], opts=["be"])
 def test_multi(cijoe, device, be_opts, cli_args):
+    if be_opts["be"] == "spdk":
+        pytest.skip(reason="[be=spdk] does not support repeatedly enumerating")
     if be_opts["be"] == "ramdisk":
         pytest.skip(reason="[be=ramdisk] does not support enumeration")
 
@@ -41,21 +33,7 @@ def test_multi(cijoe, device, be_opts, cli_args):
     assert not err
 
 
-def test_multi_all_be(cijoe):
-    XnvmeDriver.kernel_attach(cijoe)
-    err, _ = cijoe.run("xnvme_tests_enum multi --count 4")
-    assert not err
-
-    XnvmeDriver.kernel_detach(cijoe)
-    err, _ = cijoe.run("xnvme_tests_enum multi --count 4")
-    assert not err
-
-
-def test_backend(cijoe):
-    XnvmeDriver.kernel_attach(cijoe)
-    err, _ = cijoe.run("xnvme_tests_enum backend")
-    assert not err
-
-    XnvmeDriver.kernel_detach(cijoe)
+@xnvme_parametrize(labels=["dev"], opts=["be"])
+def test_backend(cijoe, device, be_opts, cli_args):
     err, _ = cijoe.run("xnvme_tests_enum backend")
     assert not err
