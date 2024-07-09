c3db27387228a84186962c4bbaf9268ad3101c2d
Nadja Brix Koch <n.koch@samsung.com>
2024-07-09 15:55:50 +0200

    feat(cijoe/latency): add barplot for qd1
    Added a bar plot in the xNVMe latency report for IO depth 1 and IO size 4k
    
    Signed-off-by: Nadja Brix Koch <n.koch@samsung.com>

--- cijoe/scripts/latency_reporter.py
+++ cijoe/scripts/latency_reporter.py
@@ -70,7 +70,7 @@ def main(args, cijoe, step):
     template_env = jinja2.Environment(loader=template_loader)
 
     # plots for variable IO size and IO depths
-    plot_paths = plot_path.glob("fio_*plot*.png")
+    plot_paths = plot_path.glob("fio_*plot_scalability*.png")
 
     def path_to_object(path_path):
         match = re.match(PLOT_PATH_REGEX, path_path.name)
@@ -85,10 +85,24 @@ def main(args, cijoe, step):
     for plot in plots:
         plot_groups[plot["group"]][plot["type"]] = plot["path"]
 
+    # plot for IO depth 1
+    qd1_pngs = list(plot_path.glob("fio_*qd1*.png"))
+    if len(qd1_pngs) != 1:
+        log.error(f"Unexpected paths: {qd1_pngs}")
+        return errno.EINVAL
+    qd1_plot = qd1_pngs[0].name
+
     # Read the report-template, populate it, then store it in the artifacts directory
     template = template_env.get_template("latency.jinja2.rst")
     with body_path.open("w") as body:
-        body.write(template.render({"plots": plot_groups}))
+        body.write(
+            template.render(
+                {
+                    "plots": plot_groups,
+                    "qd1_plot": qd1_plot,
+                }
+            )
+        )
 
     err, _ = cijoe.run_local(
         f"rst2pdf {body_path}"
