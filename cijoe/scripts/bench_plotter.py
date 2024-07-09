88fcd85df1e9f8162f1e167739bda64ab9bee619
Nadja Brix Koch <n.koch@samsung.com>
2024-07-09 15:04:50 +0200

    fix(cijoe/bench): split normalization and plotter
    Split the plotter scripts in two, to be able to reuse the normalization logic for plotting in both the "xNVMe in SPDK" and the xNVMe latency report.
    
    Signed-off-by: Nadja Brix Koch <n.koch@samsung.com>

--- cijoe/scripts/bench_plotter.py
+++ cijoe/scripts/bench_plotter.py
@@ -33,200 +33,13 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-from cijoe.core.analyser import to_base_unit
 from cijoe.core.resources import dict_from_yamlfile
-from cijoe.fio.wrapper import dict_from_fio_output_file
 
-JSON_DUMP = {"indent": 4}
-
-FIO_COMPOUND_FILENAME = "fio-output-compound.json"
 FIO_OUTPUT_NORMALIZED_FILENAME = "fio-output-normalize.json"
-
-BDEVPERF_OUTPUT_PREFIX = "bdevperf-output_"
 BDEVPERF_OUTPUT_NORMALIZED_FILENAME = "bdevperf-output-normalized.json"
-OUTPUT_REGEX = r".*BS=(?P<bs>.*)_IODEPTH=(?P<iodepth>\d+)_LABEL=(?P<label>.*)_\d+.txt"
-LINE_REGEX = r".*Total\s+\:\s+(?P<iops>\d+\.\d+)\s+\s+(?P<bwps>\d+\.\d+)\s+\d+\.\d+\s+\d+\.\d+\s+(?P<lat>\d+\.\d+).*"
 
 
-def extract_bdevperf(args, cijoe, step):
-    """
-    Traverse 'args.output' for 'bdevperf-output_*.txt' files, and create:
-
-    * A 'bdevperf-output_*.json' per 'bdevperf-output_*.txt'
-    * A single "compound" 'bdevperf-output-compound.json' containing all the discovered
-      JSON-documents with the file.stem as key.
-    """
-
-    search = step.get("with", {}).get("path", args.output)
-    if not search:
-        return errno.EINVAL
-
-    collection = []
-
-    search = Path(search)
-    for path in search.rglob(f"{BDEVPERF_OUTPUT_PREFIX}*.txt"):
-        with path.open() as ofile:
-            match = re.match(OUTPUT_REGEX, str(path))
-
-            sample = {
-                "ctx": {},
-                "iops": 0,
-                "bwps": 0,
-                "lat": 0,
-                "stddev": 0,
-            }
-
-            for ctx in ["bs", "iodepth", "label"]:
-                if ctx == "iodepth":
-                    sample["ctx"][ctx] = int(match.group(ctx))
-                else:
-                    sample["ctx"][ctx] = match.group(ctx)
-            sample["ctx"]["name"] = sample["ctx"]["label"]
-
-            for line in ofile.readlines():
-                metrics = re.match(LINE_REGEX, line)
-                if metrics:
-                    for metric in ["iops", "bwps"]:
-                        sample[metric] = float(metrics.group(metric))
-
-            m = hashlib.md5()
-            m.update(
-                str.encode(
-                    "".join(
-                        [
-                            f"{key}={val}"
-                            for key, val in sorted(sample["ctx"].items())
-                            if key not in ["timestamp", "stem", "iodepth"]
-                        ]
-                    )
-                )
-            )
-
-            collection.append((str(m.hexdigest()), sample))
-
-    artifacts = search / "artifacts"
-
-    with (artifacts / BDEVPERF_OUTPUT_NORMALIZED_FILENAME).open("w") as jfd:
-        json.dump(collection, jfd, **JSON_DUMP)
-
-    return 0
-
-
-def extract(args, cijoe, step):
-    """
-    Traverse 'args.output' for 'fio-output_*.txt' files, and create:
-
-    * A 'fio-output_*.json' per 'fio-output_*.txt'
-    * A single "compound" 'fio-output-compound.json' containing all the discovered
-      JSON-documents with the file.stem as key.
-    """
-
-    search = step.get("with", {}).get("path", args.output)
-    if not search:
-        return errno.EINVAL
-
-    compound = {}
-
-    search = Path(search)
-    for path in search.rglob("fio-output_*.txt"):
-        jsondict = dict_from_fio_output_file(path)
-        with path.with_suffix(".json").open("w") as jfd:
-            json.dump(jsondict, jfd, **JSON_DUMP)
-
-        compound[path.stem] = jsondict
-
-    artifacts = search / "artifacts"
-
-    with (artifacts / FIO_COMPOUND_FILENAME).open("w") as jfd:
-        json.dump(compound, jfd, **JSON_DUMP)
-        log.info(f"wrote: {jfd.name}")
-
-    return 0
-
-
-def normalize(args, cijoe, step):
-    """Transform the "compound" fio output into series of 'normalized' data"""
-
-    def contextualize(fio_output: dict, stem) -> dict:
-        """Helper function for the transformation of output"""
-
-        assert len(fio_output["jobs"]) == 1
-
-        job = fio_output["jobs"][0]
-        options = job["job options"]
-
-        context = {
-            "name": options["name"],
-            "timestamp": fio_output["timestamp"],
-            "ioengine": options["ioengine"],
-            "bs": options["bs"],
-            "rw": options["rw"],
-            "size": options["size"],
-            "iodepth": int(options["iodepth"]),
-            "filename": options["filename"],
-            "stem": stem,
-        }
-
-        context.update(
-            {
-                key: options[key]
-                for key in [
-                    "spdk_conf",
-                    "xnvme_be",
-                    "xnvme_async",
-                    "xnvme_sync",
-                    "xnvme_admin",
-                    "xnvme_dev",
-                ]
-                if key in options
-            }
-        )
-
-        metrics = {
-            "ctx": context,
-            "iops": to_base_unit(job["read"]["iops_mean"], ""),
-            "bwps": to_base_unit(job["read"]["bw_mean"], "KiB"),
-            "lat": job["read"]["lat_ns"]["mean"],
-            "stddev": to_base_unit(job["read"]["lat_ns"]["stddev"], ""),
-        }
-
-        m = hashlib.md5()
-        m.update(
-            str.encode(
-                "".join(
-                    [
-                        f"{key}={val}"
-                        for key, val in sorted(context.items())
-                        if key not in ["timestamp", "stem", "iodepth"]
-                    ]
-                )
-            )
-        )
-
-        return str(m.hexdigest()), metrics
-
-    search = step.get("with", {}).get("path", args.output)
-    if not search:
-        return errno.EINVAL
-
-    paths = list(Path(search).rglob(FIO_COMPOUND_FILENAME))
-    if len(paths) != 1:
-        log.error(f"Unexpected paths: {paths}")
-        return errno.EINVAL
-
-    compound_path = paths[0]
-    with compound_path.open() as jfd:
-        compound = json.load(jfd)
-
-    collection = [contextualize(results, stem) for stem, results in compound.items()]
-
-    with compound_path.with_name(FIO_OUTPUT_NORMALIZED_FILENAME).open("w") as jfd:
-        json.dump(collection, jfd, **JSON_DUMP)
-
-    return 0
-
-
-def data_as_a_function_of(data, x="iodepth", y="iops"):
+def data_as_a_function_of(data, x="iodepth", y="iops", filter=lambda _: True):
     """Organize and label data with 'y' points as a function of 'x' points"""
 
     samples = {}
@@ -433,21 +246,9 @@ def barplot(args, cijoe, step):
 
 def main(args, cijoe, step):
     try:
-        search = step.get("with", {}).get("path", args.output)
-        tool = step.get("with", {}).get("tool", "fio")
-
-        if not search:
-            return errno.EINVAL
-
-        if tool == "fio":
-            funcs = [extract, normalize, lineplot, barplot]
-        elif tool == "bdevperf":
-            funcs = [extract_bdevperf, lineplot, barplot]
-
-        for func in funcs:
-            err = func(args, cijoe, step)
-            if err:
-                return err
+        err = lineplot(args, cijoe, step)
+        if err:
+            return err
     except Exception as exc:
         log.error(f"Something failed({exc})")
         log.error("".join(traceback.format_exception(None, exc, exc.__traceback__)))
--- cijoe/scripts/latency_plotter.py
+++ cijoe/scripts/latency_plotter.py
@@ -27,128 +27,14 @@ from pathlib import Path
 from typing import Dict, List
 
 import matplotlib.pyplot as plt
-from cijoe.core.analyser import to_base_unit
 from cijoe.core.resources import dict_from_yamlfile
-from cijoe.fio.wrapper import dict_from_fio_output_file
 
-JSON_DUMP = {"indent": 4}
-
-FIO_COMPOUND_FILENAME = "fio-output-compound.json"
-FIO_OUTPUT_NORMALIZED_FILENAME = "fio-output-normalize.json"
+FIO_OUTPUT_NORMALIZED_FILENAME = "fio-output-normalized.json"
 
 Data = Dict[str, Dict[str, List[float | int] | str]]
 
 
-def extract_fio(args, cijoe, step):
-    """
-    Traverse 'args.output' for 'fio-output_*.txt' files, and create:
-
-    * A 'fio-output_*.json' per 'fio-output_*.txt'
-    * A single "compound" 'fio-output-compound.json' containing all the discovered
-      JSON-documents with the file.stem as key.
-    """
-
-    search = step.get("with", {}).get("path", args.output)
-    if not search:
-        return errno.EINVAL
-
-    compound = {}
-
-    search = Path(search)
-    for path in search.rglob("fio-output_*.txt"):
-        jsondict = dict_from_fio_output_file(path)
-        with path.with_suffix(".json").open("w") as jfd:
-            json.dump(jsondict, jfd, **JSON_DUMP)
-
-        compound[path.stem] = jsondict
-    artifacts = search / "artifacts"
-
-    with (artifacts / FIO_COMPOUND_FILENAME).open("w") as jfd:
-        json.dump(compound, jfd, **JSON_DUMP)
-        log.info(f"wrote: {jfd.name}")
-
-    return 0
-
-
-def normalize(args, cijoe, step):
-    """Transform the "compound" fio output into series of 'normalized' data"""
-
-    def contextualize(fio_output: dict, stem) -> dict:
-        """Helper function for the transformation of output"""
-
-        assert len(fio_output["jobs"]) == 1
-
-        job = fio_output["jobs"][0]
-        options = job["job options"]
-
-        context = {
-            "name": options["name"],
-            "timestamp": fio_output["timestamp"],
-            "ioengine": options["ioengine"],
-            "bs": options["bs"],
-            "rw": options["rw"],
-            "size": options["size"],
-            "iodepth": int(options["iodepth"]),
-            "filename": options["filename"],
-            "stem": stem,
-        }
-
-        context.update(
-            {
-                key: options[key]
-                for key in [
-                    "spdk_conf",
-                    "xnvme_be",
-                    "xnvme_async",
-                    "xnvme_sync",
-                    "xnvme_admin",
-                    "xnvme_dev",
-                ]
-                if key in options
-            }
-        )
-
-        metrics = {
-            "ctx": context,
-            "iops": to_base_unit(job["read"]["iops_mean"], ""),
-            "bwps": to_base_unit(job["read"]["bw_mean"], "KiB"),
-            "lat": job["read"]["lat_ns"]["mean"],
-            "stddev": to_base_unit(job["read"]["lat_ns"]["stddev"], ""),
-        }
-
-        md5_basis = [
-            f"{key}={val}"
-            for key, val in sorted(context.items())
-            if key not in {"timestamp", "stem", "iodepth", "filename"}
-        ]
-        md5_key = "".join(md5_basis)
-        m = hashlib.md5()
-        m.update(str.encode(md5_key))
-
-        return str(m.hexdigest()), metrics
-
-    search = step.get("with", {}).get("path", args.output)
-    if not search:
-        return errno.EINVAL
-
-    paths = list(Path(search).rglob(FIO_COMPOUND_FILENAME))
-    if len(paths) != 1:
-        log.error(f"Unexpected paths: {paths}")
-        return errno.EINVAL
-
-    compound_path = paths[0]
-    with compound_path.open() as jfd:
-        compound = json.load(jfd)
-
-    collection = [contextualize(results, stem) for stem, results in compound.items()]
-
-    with compound_path.with_name(FIO_OUTPUT_NORMALIZED_FILENAME).open("w") as jfd:
-        json.dump(collection, jfd, **JSON_DUMP)
-
-    return 0
-
-
-def data_as_a_function_of(data, x="iodepth", y="iops") -> Data:
+def data_as_a_function_of(data, x="iodepth", y="lat", filter=lambda _: True) -> Data:
     """Organize and label data with 'y' points as a function of 'x' points"""
 
     samples = {}
@@ -288,17 +174,9 @@ def line_plot(args, cijoe, step):
 
 def main(args, cijoe, step):
     try:
-        search = step.get("with", {}).get("path", args.output)
-
-        if not search:
-            return errno.EINVAL
-
-        funcs = [extract_fio, normalize, line_plot]
-
-        for func in funcs:
-            err = func(args, cijoe, step)
-            if err:
-                return err
+        err = line_plot(args, cijoe, step)
+        if err:
+            return err
     except Exception as exc:
         log.error(f"Something failed({exc})")
         log.error("".join(traceback.format_exception(None, exc, exc.__traceback__)))
