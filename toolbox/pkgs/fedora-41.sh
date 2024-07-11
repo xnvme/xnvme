6cc4c231502e1f92e3752a57214fae94a7077b35
Karl Bonde Torp <k.torp@samsung.com>
2024-07-11 15:36:03 +0200

    feat(build): bump libvfn to v5.0.0
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- toolbox/pkgs/fedora-41.sh
+++ toolbox/pkgs/fedora-41.sh
@@ -49,7 +49,7 @@ dnf install -y \
 git clone https://github.com/SamsungDS/libvfn.git toolbox/third-party/libvfn/repository
 
 pushd toolbox/third-party/libvfn/repository
-git checkout v4.0.1
+git checkout v5.0.0
 meson setup builddir -Dlibnvme="disabled" -Ddocs="disabled" --buildtype=release --prefix=/usr
 meson compile -C builddir
 meson install -C builddir
