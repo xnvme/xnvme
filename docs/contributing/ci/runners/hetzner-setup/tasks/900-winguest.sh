263e51552c0fd951be7f11efbb44d5d855e3558f
Simon A. F. Lund <simon.lund@samsung.com>
2025-09-08 09:01:24 +0200

    fix(docs/ci): add task preparing for windows guest
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ docs/contributing/ci/runners/hetzner-setup/tasks/900-winguest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eux
+
+mkdir -p /home/ghr/system_imaging/{disk,fd}
+cp /usr/share/OVMF/OVMF_CODE_4M.ms.fd /home/ghr/system_imaging/fd/win11_VARS.fd
+chown -R ghr:ghr /home/ghr/system_imaging
