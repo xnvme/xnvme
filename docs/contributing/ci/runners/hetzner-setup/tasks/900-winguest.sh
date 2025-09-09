f4ccb66cbc91d8880018a042c876d3f1026d2b80
Simon A. F. Lund <simon.lund@samsung.com>
2025-09-09 14:44:42 +0200

    fix(docs/ci): add virtio download as part of prep
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- docs/contributing/ci/runners/hetzner-setup/tasks/900-winguest.sh
+++ docs/contributing/ci/runners/hetzner-setup/tasks/900-winguest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eux
 
-mkdir -p /home/ghr/system_imaging/{disk,fd}
+mkdir -p /home/ghr/system_imaging/{disk,fd,iso}
+wget -p /home/ghr/system_imaging/iso/ https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.271-1/virtio-win.iso
 cp /usr/share/OVMF/OVMF_VARS_4M.ms.fd /home/ghr/system_imaging/fd/win11_VARS.fd
 chown -R ghr:ghr /home/ghr/system_imaging
