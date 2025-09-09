a75dcbad7012dcd1fbc436e2b60ffe94e1d1da16
Simon A. F. Lund <simon.lund@samsung.com>
2025-09-09 14:39:39 +0200

    fix(docs/ci): change the flash-image
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- docs/contributing/ci/runners/hetzner-setup/tasks/900-winguest.sh
+++ docs/contributing/ci/runners/hetzner-setup/tasks/900-winguest.sh
@@ -2,5 +2,5 @@
 set -eux
 
 mkdir -p /home/ghr/system_imaging/{disk,fd}
-cp /usr/share/OVMF/OVMF_CODE_4M.ms.fd /home/ghr/system_imaging/fd/win11_VARS.fd
+cp /usr/share/OVMF/OVMF_VARS_4M.ms.fd /home/ghr/system_imaging/fd/win11_VARS.fd
 chown -R ghr:ghr /home/ghr/system_imaging
