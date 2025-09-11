682461449d558f7b5025d3a797745faa4e5609d0
Simon A. F. Lund <simon.lund@samsung.com>
2025-09-11 11:08:30 +0200

    fix(docs/ci): change wget command
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- docs/contributing/ci/runners/hetzner-setup/tasks/900-winguest.sh
+++ docs/contributing/ci/runners/hetzner-setup/tasks/900-winguest.sh
@@ -2,6 +2,13 @@
 set -eux
 
 mkdir -p /home/ghr/system_imaging/{disk,fd,iso}
-wget -p /home/ghr/system_imaging/iso/ https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.271-1/virtio-win.iso
+wget \
+ 'https://u489897-sub1:PublicAccess1!@u489897-sub1.your-storagebox.de/iso/virtio-win.iso' \
+ -O /home/ghr/system_imaging/iso/virtio-win.iso
+
 cp /usr/share/OVMF/OVMF_VARS_4M.ms.fd /home/ghr/system_imaging/fd/win11_VARS.fd
 chown -R ghr:ghr /home/ghr/system_imaging
+
+echo "# Here are the system_imaging files - BEGIN"
+find /home/ghr/system_imaging
+echo "# Here are the system_imaging files - END"
