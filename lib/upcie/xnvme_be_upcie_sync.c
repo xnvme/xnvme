8b67324f79f34fa6739d90965b41d9bc9f7de816
Nadja Brix Koch <n.koch@samsung.com>
2026-03-17 09:37:13 +0100

    refactor(lib/upcie): cleanup sync_cmd_io + iov functions
    The setup and mid-line casting in these functions made them difficult
    to read. I have followed the structure of the sync_cmd_admin function.
    
    Signed-off-by: Nadja Brix Koch <n.koch@samsung.com>

--- lib/upcie/xnvme_be_upcie_sync.c
+++ lib/upcie/xnvme_be_upcie_sync.c
@@ -15,6 +15,8 @@ xnvme_be_upcie_sync_cmd_io(struct xnvme_cmd_ctx *ctx, void *dbuf, size_t dbuf_nb
 {
 	struct xnvme_be_upcie_state *state = (void *)ctx->dev->be.state;
 	struct nvme_controller *ctrl = state->ctrlr->ctrl;
+	struct nvme_command *cmd = (struct nvme_command *)&ctx->cmd;
+	struct nvme_completion *cpl = (struct nvme_completion *)&ctx->cpl;
 	int err;
 
 	switch (ctx->cmd.common.opcode) {
@@ -34,28 +36,20 @@ xnvme_be_upcie_sync_cmd_io(struct xnvme_cmd_ctx *ctx, void *dbuf, size_t dbuf_nb
 	}
 
 	if (dbuf) {
-		err = nvme_qpair_submit_sync_contig_prps(
-			&state->ctrlr->sync, &g_upcie_rte.heap, dbuf, dbuf_nbytes,
-			(struct nvme_command *)&ctx->cmd, ctrl->timeout_ms,
-			(struct nvme_completion *)&ctx->cpl);
-		if (err || xnvme_cmd_ctx_cpl_status(ctx)) {
-			XNVME_DEBUG("FAILED: nvme_qpair_submit_sync_contig_prps(); err(%d); "
-				    "sc(%d); sct(%d)",
-				    err, ctx->cpl.status.sc, ctx->cpl.status.sct);
-			return err;
-		}
+		err = nvme_qpair_submit_sync_contig_prps(&state->ctrlr->sync, &g_upcie_rte.heap,
+							 dbuf, dbuf_nbytes, cmd, ctrl->timeout_ms,
+							 cpl);
 	} else {
-		err = nvme_qpair_submit_sync(&state->ctrlr->sync, (struct nvme_command *)&ctx->cmd,
-					     ctrl->timeout_ms,
-					     (struct nvme_completion *)&ctx->cpl);
-		if (err || xnvme_cmd_ctx_cpl_status(ctx)) {
-			XNVME_DEBUG("FAILED: nvme_qpair_submit_sync(); err(%d); sc(%d); sct(%d)",
-				    err, ctx->cpl.status.sc, ctx->cpl.status.sct);
-			return err;
-		}
+		err = nvme_qpair_submit_sync(&state->ctrlr->sync, cmd, ctrl->timeout_ms, cpl);
 	}
 
-	return err;
+	if (err || xnvme_cmd_ctx_cpl_status(ctx)) {
+		XNVME_DEBUG("FAILED: nvme_qpair_submit_sync(); err(%d); sc(%d); sct(%d)", err,
+			    ctx->cpl.status.sc, ctx->cpl.status.sct);
+		return err;
+	}
+
+	return 0;
 }
 
 int
@@ -65,6 +59,8 @@ xnvme_be_upcie_sync_cmd_iov(struct xnvme_cmd_ctx *ctx, struct iovec *dvec, size_
 {
 	struct xnvme_be_upcie_state *state = (void *)ctx->dev->be.state;
 	struct nvme_controller *ctrl = state->ctrlr->ctrl;
+	struct nvme_command *cmd = (struct nvme_command *)&ctx->cmd;
+	struct nvme_completion *cpl = (struct nvme_completion *)&ctx->cpl;
 	int err;
 
 	switch (ctx->cmd.common.opcode) {
@@ -85,27 +81,18 @@ xnvme_be_upcie_sync_cmd_iov(struct xnvme_cmd_ctx *ctx, struct iovec *dvec, size_
 
 	if (dvec) {
 		err = nvme_qpair_submit_sync_iov_prps(&state->ctrlr->sync, &g_upcie_rte.heap, dvec,
-						      dvec_cnt, (struct nvme_command *)&ctx->cmd,
-						      ctrl->timeout_ms,
-						      (struct nvme_completion *)&ctx->cpl);
-		if (err || xnvme_cmd_ctx_cpl_status(ctx)) {
-			XNVME_DEBUG("FAILED: nvme_qpair_submit_sync_iov_prps(); err(%d); sc(%d); "
-				    "sct(%d)",
-				    err, ctx->cpl.status.sc, ctx->cpl.status.sct);
-			return err;
-		}
+						      dvec_cnt, cmd, ctrl->timeout_ms, cpl);
 	} else {
-		err = nvme_qpair_submit_sync(&state->ctrlr->sync, (struct nvme_command *)&ctx->cmd,
-					     ctrl->timeout_ms,
-					     (struct nvme_completion *)&ctx->cpl);
-		if (err || xnvme_cmd_ctx_cpl_status(ctx)) {
-			XNVME_DEBUG("FAILED: nvme_qpair_submit_sync(); err(%d); sc(%d); sct(%d)",
-				    err, ctx->cpl.status.sc, ctx->cpl.status.sct);
-			return err;
-		}
+		err = nvme_qpair_submit_sync(&state->ctrlr->sync, cmd, ctrl->timeout_ms, cpl);
 	}
 
-	return err;
+	if (err || xnvme_cmd_ctx_cpl_status(ctx)) {
+		XNVME_DEBUG("FAILED: nvme_qpair_submit_sync(); err(%d); sc(%d); sct(%d)", err,
+			    ctx->cpl.status.sc, ctx->cpl.status.sct);
+		return err;
+	}
+
+	return 0;
 }
 
 #endif
