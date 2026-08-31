e34a3eaeb42c97647b65e4c259796d0389072953
James Swaro <j.swaro@samsung.com>
2026-08-31 08:38:14 +0000

    fixup! moved nvme debug code
    Signed-off-by: James Swaro <j.swaro@samsung.com>

--- lib/nvmf/xnvme_be_nvmf_debug.h
+++ lib/nvmf/xnvme_be_nvmf_debug.h
@@ -6,4 +6,10 @@
 int
 _xnvme_print_error_code(struct xnvme_spec_cpl *cpl);
 
+static inline void _print_nvme_completion(struct xnvme_spec_cpl *cpl)
+{
+	XNVME_DEBUG("INFO: NVMe Completion - cid: %u, sc: %u, sct: %u",
+		    cpl->cid, cpl->status.sc, cpl->status.sct);
+}
+
 #endif // _INTERNAL_XNVME_BE_NVMF_DEBUG_H
