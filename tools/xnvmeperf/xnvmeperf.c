988cb79c9de8a5cc699488a132578e580b525ef9
Nadja Brix Koch <n.koch@samsung.com>
2026-09-22 10:20:36 +0200

    fix(xnvmeperf): stop report loop early if threads are finished
    If all threads exited early and --report_freq was given, the main thread
    was kept awake for reporting for the full runtime.
    
    Signed-off-by: Nadja Brix Koch <n.koch@samsung.com>

--- tools/xnvmeperf/xnvmeperf.c
+++ tools/xnvmeperf/xnvmeperf.c
@@ -42,6 +42,7 @@ struct xnvmeperf_thread {
 	double elapsed;
 	struct xnvme_dev **devs;
 	int ndevs;
+	int done;
 };
 
 #ifndef XNVME_RAND_R_ENABLED
@@ -327,6 +328,7 @@ thread_fn(void *arg)
 		err = xnvme_buf_fill(job->buf, args->iosize, "anum");
 		if (err) {
 			xnvme_cli_perr("Failed: xnvme_buf_fill()", err);
+			thread->done = 1;
 			return NULL;
 		}
 	}
@@ -384,6 +386,7 @@ thread_fn(void *arg)
 
 	xnvme_timer_stop(&timer);
 	thread->elapsed = xnvme_timer_elapsed_secs(&timer);
+	thread->done = 1;
 
 	return NULL;
 }
@@ -709,6 +712,14 @@ xnvmeperf_run(struct xnvmeperf_args *args)
 		while (1) {
 			struct timespec ts;
 			uint64_t elapsed, wakeup;
+			int running = 0;
+
+			for (uint16_t i = 0; i < args->ncpus; i++) {
+				running += !threads[i].done;
+			}
+			if (!running) {
+				break;
+			}
 
 			xnvme_timer_stop(&timer);
 			elapsed = xnvme_timer_elapsed_nsecs(&timer);
